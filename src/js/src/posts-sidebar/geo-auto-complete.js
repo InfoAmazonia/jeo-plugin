2d6468f2227529cd24d0f3966487711c8f0b9e87
leogermani <leogermani@gmail.com>
2019-10-22 10:12:05 -0300

    coding standards commit

--- src/js/src/posts-sidebar/geo-auto-complete.js
+++ src/js/src/posts-sidebar/geo-auto-complete.js
@@ -5,99 +5,98 @@ import { Spinner } from "@wordpress/components";
 
 
 class JeoGeoAutoComplete extends React.Component {
-    
-	
+
 	constructor() {
 		super();
 		this.state = {
 			value: '',
 			suggestions: [],
-            isLoading: false
+			isLoading: false
 		}
-        this.onChange = this.onChange.bind(this);
-        this.onSuggestionsFetchRequested = this.onSuggestionsFetchRequested.bind(this);
-        this.onSuggestionsClearRequested = this.onSuggestionsClearRequested.bind(this);
-        this.getSuggestionValue = this.getSuggestionValue.bind(this);
-        this.renderSuggestion = this.renderSuggestion.bind(this);
-        this.onSuggestionSelected = this.onSuggestionSelected.bind(this);
-        this.shouldRenderSuggestions = this.shouldRenderSuggestions.bind(this);
-        
-        this.debouncedLoadSuggestions = _.debounce(this.onSuggestionsFetchRequested, 500);
-        
+		this.onChange = this.onChange.bind(this);
+		this.onSuggestionsFetchRequested = this.onSuggestionsFetchRequested.bind(this);
+		this.onSuggestionsClearRequested = this.onSuggestionsClearRequested.bind(this);
+		this.getSuggestionValue = this.getSuggestionValue.bind(this);
+		this.renderSuggestion = this.renderSuggestion.bind(this);
+		this.onSuggestionSelected = this.onSuggestionSelected.bind(this);
+		this.shouldRenderSuggestions = this.shouldRenderSuggestions.bind(this);
+
+		this.debouncedLoadSuggestions = _.debounce(this.onSuggestionsFetchRequested, 500);
+
 	}
-    
-    onChange(event, { newValue }) {
-        this.setState({
-            value: newValue
-        });
-    };
-    
-    getSuggestionValue(suggestion) {
-        return suggestion.display_name;
-        
-    };
-    
-    onSuggestionSelected(event, { suggestion, suggestionValue, suggestionIndex, sectionIndex, method }) {
-        this.props.onSelect(suggestion);
-    }
-    
-    renderSuggestion(suggestion) {
-        return (
-            <span>{suggestion.display_name}</span>
-        )
-    };
-    
-    onSuggestionsFetchRequested({ value }) {
-        this.setState( { isLoading: true } );
-        fetch(jeo.ajax_url + '?action=jeo_geocode&search=' + value)
-            .then( response => {
-                return response.json();
-            } )
-            .then( result => {
-                this.setState( { suggestions: result, isLoading: false } );
-            } );
-    };
-    
-    shouldRenderSuggestions(value) {
-      return value.trim().length > 2;
-    }
-    
-    onSuggestionsClearRequested() {
-        this.setState({
-            suggestions: []
-        });
-    };
-    
-    render() {
-        const { value, suggestions } = this.state;
-        // Autosuggest will pass through all these props to the input.
-        const inputProps = {
-            placeholder: 'Busca por endereço',
-            value,
-            onChange: this.onChange
-        };
-        
-        // Finally, render it!
-        return (
-            <div>
-            
-            <Autosuggest
-            suggestions={suggestions}
-            onSuggestionsFetchRequested={this.debouncedLoadSuggestions}
-            onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-            getSuggestionValue={this.getSuggestionValue}
-            renderSuggestion={this.renderSuggestion}
-            onSuggestionSelected={this.onSuggestionSelected}
-            shouldRenderSuggestions={this.shouldRenderSuggestions}
-            inputProps={inputProps}
-            />
-            { this.state.isLoading && 
-                <Spinner />
-            }
-            </div>
-        );
-    }
-	
+
+	onChange(event, { newValue }) {
+		this.setState({
+			value: newValue
+		});
+	};
+
+	getSuggestionValue(suggestion) {
+		return suggestion.display_name;
+
+	};
+
+	onSuggestionSelected(event, { suggestion, suggestionValue, suggestionIndex, sectionIndex, method }) {
+		this.props.onSelect(suggestion);
+	}
+
+	renderSuggestion(suggestion) {
+		return (
+			<span>{suggestion.display_name}</span>
+		)
+	};
+
+	onSuggestionsFetchRequested({ value }) {
+		this.setState( { isLoading: true } );
+		fetch(jeo.ajax_url + '?action=jeo_geocode&search=' + value)
+			.then( response => {
+				return response.json();
+			} )
+			.then( result => {
+				this.setState( { suggestions: result, isLoading: false } );
+			} );
+	};
+
+	shouldRenderSuggestions(value) {
+	return value.trim().length > 2;
+	}
+
+	onSuggestionsClearRequested() {
+		this.setState({
+			suggestions: []
+		});
+	};
+
+	render() {
+		const { value, suggestions } = this.state;
+		// Autosuggest will pass through all these props to the input.
+		const inputProps = {
+			placeholder: 'Busca por endereço',
+			value,
+			onChange: this.onChange
+		};
+
+		// Finally, render it!
+		return (
+			<div>
+
+			<Autosuggest
+			suggestions={suggestions}
+			onSuggestionsFetchRequested={this.debouncedLoadSuggestions}
+			onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+			getSuggestionValue={this.getSuggestionValue}
+			renderSuggestion={this.renderSuggestion}
+			onSuggestionSelected={this.onSuggestionSelected}
+			shouldRenderSuggestions={this.shouldRenderSuggestions}
+			inputProps={inputProps}
+			/>
+			{ this.state.isLoading &&
+				<Spinner />
+			}
+			</div>
+		);
+	}
+
 }
 
 export default JeoGeoAutoComplete;
--- src/js/src/posts-sidebar/geo-posts.js
+++ src/js/src/posts-sidebar/geo-posts.js
@@ -6,104 +6,104 @@ import { Button } from "@wordpress/components";
 import { __ } from "@wordpress/i18n";
 
 class JeoGeocodePosts extends React.Component {
-    constructor() {
-      super();
-      const metadata = wp.data.select('core/editor').getCurrentPost().meta;
-    //   console.log(metadata);
-      const marker = metadata._geocode_lat && metadata._geocode_lon ? [ metadata._geocode_lat, metadata._geocode_lon ] : false
-      this.state = {
-        lat: metadata._geocode_lat,
-        lng: metadata._geocode_lon,
-        zoom: 13,
-        marker: marker
-      }
-      
-      this.onLocationFound = this.onLocationFound.bind(this);
-      this.onMarkerDragged = this.onMarkerDragged.bind(this);
-      this.save = this.save.bind(this);
-      
-      this.markerRef = React.createRef();
-      
-  };
-    
-    onLocationFound(location) {
-        console.log(location);
-        this.setState({
-            marker: [
-                location.lat,
-                location.lon
-            ],
-            lat: location.lat,
-            lng: location.lon,
-        });
-    };
-    
-    onMarkerDragged() {
-        const marker = this.markerRef.current;
-        const latLng = marker.leafletElement.getLatLng();
-        this.setState({
-            marker: [
-                latLng.lat,
-                latLng.lng
-            ],
-            lat: latLng.lat,
-            lng: latLng.lng,
-        });
-        
-    }
-    
-    save() {
-        const lat = this.state.marker[0];
-        const lon = this.state.marker[1];
-        
-        wp.data.dispatch('core/editor').editPost({meta: {
-            _geocode_lat: lat,
-            _geocode_lon: lon,
-        }});
-        
-        //this.props.onSaveLocation();
-    }
-    
-  render() {
-    const position = [this.state.lat, this.state.lng];
-    return (
-        <fragment>
-            <p>{__('Search your location', 'jeo')}</p>
-            <JeoGeoAutoComplete onSelect={this.onLocationFound} />
-            <div id="geocode-map-container">
-                <LeafletMap center={position} zoom={this.state.zoom}>
-                <TileLayer
-                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                  url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
-                />
-                {this.state.marker && (
-                    
-                    <Marker 
-                        draggable={true} 
-                        onDragend={this.onMarkerDragged} 
-                        position={[this.state.marker[0], this.state.marker[1]]}
-                        ref={this.markerRef}
-                        >
-                    </Marker>
-                    
-                )}
-                
-              </LeafletMap>
-              
-              <Button isDefault onClick={ this.save() }>
-                  {__('Save', 'jeo')}
-              </Button>
-              
-              <Button onClick={ this.props.onCancel }>
-                  {__('Cancel', 'jeo')}
-              </Button>
-              
-            </div>
-            
-        </fragment>
-    );
-   
-}
+	constructor() {
+		super();
+		const metadata = wp.data.select('core/editor').getCurrentPost().meta;
+		// console.log(metadata);
+		const marker = metadata._geocode_lat && metadata._geocode_lon ? [ metadata._geocode_lat, metadata._geocode_lon ] : false
+		this.state = {
+			lat: metadata._geocode_lat,
+			lng: metadata._geocode_lon,
+			zoom: 13,
+			marker: marker
+		}
+
+		this.onLocationFound = this.onLocationFound.bind(this);
+		this.onMarkerDragged = this.onMarkerDragged.bind(this);
+		this.save = this.save.bind(this);
+
+		this.markerRef = React.createRef();
+
+	};
+
+	onLocationFound(location) {
+		console.log(location);
+		this.setState({
+			marker: [
+				location.lat,
+				location.lon
+			],
+			lat: location.lat,
+			lng: location.lon,
+		});
+	};
+
+	onMarkerDragged() {
+		const marker = this.markerRef.current;
+		const latLng = marker.leafletElement.getLatLng();
+		this.setState({
+			marker: [
+				latLng.lat,
+				latLng.lng
+			],
+			lat: latLng.lat,
+			lng: latLng.lng,
+		});
+
+	}
+
+	save() {
+		const lat = this.state.marker[0];
+		const lon = this.state.marker[1];
+
+		wp.data.dispatch('core/editor').editPost({meta: {
+			_geocode_lat: lat,
+			_geocode_lon: lon,
+		}});
+
+		//this.props.onSaveLocation();
+	}
+
+	render() {
+		const position = [this.state.lat, this.state.lng];
+		return (
+			<fragment>
+				<p>{__('Search your location', 'jeo')}</p>
+				<JeoGeoAutoComplete onSelect={this.onLocationFound} />
+				<div id="geocode-map-container">
+					<LeafletMap center={position} zoom={this.state.zoom}>
+					<TileLayer
+					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+					url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+					/>
+					{this.state.marker && (
+
+						<Marker
+							draggable={true}
+							onDragend={this.onMarkerDragged}
+							position={[this.state.marker[0], this.state.marker[1]]}
+							ref={this.markerRef}
+							>
+						</Marker>
+
+					)}
+
+				</LeafletMap>
+
+				<Button isDefault onClick={ this.save() }>
+					{__('Save', 'jeo')}
+				</Button>
+
+				<Button onClick={ this.props.onCancel }>
+					{__('Cancel', 'jeo')}
+				</Button>
+
+				</div>
+
+			</fragment>
+		);
+
+	}
 }
 
 export default JeoGeocodePosts;
