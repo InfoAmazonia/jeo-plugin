1d33dec63ce4ad549ed0efd3904e56e1885b1789
leogermani <leogermani@gmail.com>
2019-10-22 10:12:05 -0300

    coding standards commit

--- src/js/src/posts-sidebar/index.js
+++ src/js/src/posts-sidebar/index.js
@@ -7,44 +7,44 @@ import JeoGeocodePosts from "./geo-posts";
 import { __ } from "@wordpress/i18n";
 
 const JeoGeocodePanel = class JeoGeocodePanel extends React.Component {
-    constructor() {
-      super();
-      this.state = {
-        isOpen: false
-      }
-    }
-    render() {
-          let isOpen = this.state.isOpen;
-          return (
-              <fragment>
-              <Button isDefault onClick={ () => this.setState( { isOpen: true } ) }>Open Modal</Button>
-              { isOpen && (
-                  <Modal
-                      title={__('Geolocate this post', 'jeo')}
-                      onRequestClose={ () => this.setState( { isOpen: false } ) }>
-                      <JeoGeocodePosts 
-                        onSaveLocation={ () => this.setState( { isOpen: false } ) }
-                        onCancel={ () => this.setState( { isOpen: false } ) }
-                        />
-                  </Modal>
-              ) }
-              </fragment>
-          );
-      }
-      
-      
+	constructor() {
+		super();
+		this.state = {
+			isOpen: false
+		}
+	}
+	render() {
+		let isOpen = this.state.isOpen;
+		return (
+			<fragment>
+				<Button isDefault onClick={ () => this.setState( { isOpen: true } ) }>Open Modal</Button>
+				{ isOpen && (
+					<Modal
+						title={__('Geolocate this post', 'jeo')}
+						onRequestClose={ () => this.setState( { isOpen: false } ) }>
+						<JeoGeocodePosts
+							onSaveLocation={ () => this.setState( { isOpen: false } ) }
+							onCancel={ () => this.setState( { isOpen: false } ) }
+							/>
+					</Modal>
+				) }
+			</fragment>
+		);
+	}
+
+
 };
 
 
 registerPlugin( 'myprefix-sidebar', {
-  icon: 'location-alt',
-  render: () => {
-    return (
-        <PluginDocumentSettingPanel
-          title={__('Geolocate this post', 'jeo')}
-        >
-            <JeoGeocodePanel />
-        </PluginDocumentSettingPanel>
-    )
-  }
-})
+	icon: 'location-alt',
+	render: () => {
+		return (
+			<PluginDocumentSettingPanel
+			title={__('Geolocate this post', 'jeo')}
+			>
+				<JeoGeocodePanel />
+			</PluginDocumentSettingPanel>
+		)
+	}
+});
