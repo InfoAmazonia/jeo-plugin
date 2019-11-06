19a0816f2fe52d435382de259399ce78c1012b97
leogermani <leogermani@gmail.com>
2019-11-06 08:31:58 -0300

    documentation

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 source build-config.cfg
 
+## Still have to work on this. For now, manually run "npm install" and "npm start"
+#npm ci
+#npm run build
+
 echo "Updating files in $wp_plugin_dir"
 
 rm -rf $wp_plugin_dir
