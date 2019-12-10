7c1fd7c6007c698a03bbb47c18d9e1ea28862763
Capi Etheriel <barraponto@gmail.com>
2019-12-10 00:01:00 -0300

    set up map editor modal

--- src/js/src/map-blocks/map-editor.js
+++ src/js/src/map-blocks/map-editor.js
@@ -1,37 +1,51 @@
+import { Button } from '@wordpress/components';
+import { useSelect } from '@wordpress/data';
+import { Fragment, useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import MapEditorModal from './map-editor-modal';
+import LayersTable from './layers-table';
+import './map-editor.css';
+
+const layerColumns = [
+	{ property: 'type', header: { label: __( 'Layer Type' ) } },
+	{ property: 'title', header: { label: __( 'Layer Name' ) } },
+	{ property: 'url', header: { label: __( 'Layer Source' ) } },
+	{ property: 'use', header: { label: __( 'Layer Options' ) } },
+];
+
 export default ( props ) => {
 	const { attributes, setAttributes } = props;
+	const layers = useSelect( ( select ) => select( 'jeo-layers' ).getLayers() );
+	const [ modal, setModal ] = useState( false );
 
 	return (
-		<form>
-			<div>
-				<label htmlFor="center-lat">Latitude Center:</label>
-				<input
-					id="center-lat"
-					name="center-lat"
-					value={ attributes.centerLat }
-					onChange={ ( e ) => setAttributes( { centerLat: Number( e.target.value ) } ) }
-				/>
-			</div>
-			<div>
-				<label htmlFor="center-lon">Longitude Center:</label>
-				<input
-					id="center-lon"
-					name="center-lon"
-					value={ attributes.centerLon }
-					onChange={ ( e ) => setAttributes( { centerLon: Number( e.target.value ) } ) }
-				/>
-			</div>
-			<div>
-				<label htmlFor="initial-zoom">Initial Zoom:</label>
-				<input
-					id="initial-zoom"
-					name="initial-zoom"
-					value={ attributes.initialZoom }
-					onChange={ ( e ) =>
-						setAttributes( { initialZoom: Number( e.target.value ) } )
-					}
-				/>
-			</div>
-		</form>
+		<Fragment>
+			{ modal && (
+				<MapEditorModal modal={ modal } setModal={ setModal }>
+					{ ( tab ) => {
+						switch ( tab.name ) {
+							case 'map':
+								return <p>Map Settings</p>;
+							case 'layers':
+								return <p>Edit Layers</p>;
+							case 'library':
+								return <p>Add From Library</p>;
+							case 'new-layer':
+								return <p>New layer</p>;
+						}
+					} }
+				</MapEditorModal>
+			) }
+
+			<LayersTable
+				columns={ layerColumns }
+				rows={ attributes.layers }
+				emptyMessage={ __( 'No layers have been selected for this map.' ) }
+			>
+				<Button isPrimary isLarge onClick={ () => setModal( 'layers' ) }>
+					{ __( 'Add a new layer' ) }
+				</Button>
+			</LayersTable>
+		</Fragment>
 	);
 };
