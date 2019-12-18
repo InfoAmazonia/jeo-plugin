a865954b8f4104d96e830e62d3f47c5ab8ed8a55
Capi Etheriel <barraponto@gmail.com>
2019-12-18 09:24:54 -0300

    display layers table in editor

--- src/js/src/map-blocks/layers-table.js
+++ src/js/src/map-blocks/layers-table.js
@@ -1,24 +1,37 @@
 import * as Table from 'reactabular-table';
 
+const columns = [
+	{ property: 'id', header: { label: 'ID' } },
+	{ property: 'title', header: { label: 'Name' } },
+];
+
 export default ( {
-	columns,
-	rows,
+	layers,
+	loadingLayers,
+	selectedLayers,
 	emptyMessage = null,
 	className,
 	children,
-} ) => (
-	<Table.Provider columns={ columns } className={ className } rowKey="id">
-		<Table.Header />
-		<Table.Body rows={ rows } />
-		<tfoot>
-			<tr>
-				<td colSpan={ columns.length }>
-					{ ! rows.length && emptyMessage && (
-						<p className="empty">{ emptyMessage }</p>
-					) }
-					{ children }
-				</td>
-			</tr>
-		</tfoot>
-	</Table.Provider>
-);
+} ) => {
+	if ( loadingLayers ) {
+		return <p>loading</p>;
+	}
+	const rows = layers.map( ( l ) => ( { id: l.id, title: l.title.rendered } ) );
+
+	return (
+		<Table.Provider columns={ columns } className={ className } rowKey="id">
+			<Table.Header />
+			<Table.Body rows={ rows } />
+			<tfoot>
+				<tr>
+					<td colSpan={ columns.length }>
+						{ ! rows.length && emptyMessage && (
+							<p className="empty">{ emptyMessage }</p>
+						) }
+						{ children }
+					</td>
+				</tr>
+			</tfoot>
+		</Table.Provider>
+	);
+};
