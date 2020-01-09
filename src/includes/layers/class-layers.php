daa381a521a588622f2221d74ffca79f20be7494
Capi Etheriel <barraponto@gmail.com>
2020-01-09 14:56:01 -0300

    fixes layer type schemas and exposes to js

--- src/includes/layers/class-layers.php
+++ src/includes/layers/class-layers.php
@@ -9,62 +9,74 @@ class Layers {
 	public $post_type = 'map-layer';
 
 	protected function init() {
-		add_action( 'init', [$this, 'register_post_type'] );
+		add_action( 'init', array( $this, 'register_post_type' ) );
 		$this->register_rest_meta_validation();
 	}
 
 	public function register_post_type() {
 
 		$labels = array(
-			'name' => __('Layers', 'jeo'),
-			'singular_name' => __('Layer', 'jeo'),
-			'add_new' => __('Add new layer', 'jeo'),
-			'add_new_item' => __('Add new layer', 'jeo'),
-			'edit_item' => __('Edit layer', 'jeo'),
-			'new_item' => __('New layer', 'jeo'),
-			'view_item' => __('View layer', 'jeo'),
-			'search_items' => __('Search layers', 'jeo'),
-			'not_found' => __('No layer found', 'jeo'),
-			'not_found_in_trash' => __('No layer found in the trash', 'jeo'),
-			'menu_name' => __('Layers', 'jeo')
+			'name'               => __( 'Layers', 'jeo' ),
+			'singular_name'      => __( 'Layer', 'jeo' ),
+			'add_new'            => __( 'Add new layer', 'jeo' ),
+			'add_new_item'       => __( 'Add new layer', 'jeo' ),
+			'edit_item'          => __( 'Edit layer', 'jeo' ),
+			'new_item'           => __( 'New layer', 'jeo' ),
+			'view_item'          => __( 'View layer', 'jeo' ),
+			'search_items'       => __( 'Search layers', 'jeo' ),
+			'not_found'          => __( 'No layer found', 'jeo' ),
+			'not_found_in_trash' => __( 'No layer found in the trash', 'jeo' ),
+			'menu_name'          => __( 'Layers', 'jeo' ),
 		);
 
 		$args = array(
-			'labels' => $labels,
-			'hierarchical' => true,
-			'description' => __('JEO Layers', 'jeo'),
-			'supports' => array('title', 'custom-fields'),
-			'rewrite' => array('slug' => 'layers'),
-			'show_in_rest' => true,
-			'public' => true,
-			'show_in_menu' => false,
-			'has_archive' => true,
+			'labels'              => $labels,
+			'hierarchical'        => true,
+			'description'         => __( 'JEO Layers', 'jeo' ),
+			'supports'            => array( 'title', 'custom-fields' ),
+			'rewrite'             => array( 'slug' => 'layers' ),
+			'show_in_rest'        => true,
+			'public'              => true,
+			'show_in_menu'        => true,
+			'has_archive'         => true,
 			'exclude_from_search' => true,
-			'capability_type' => 'page'
+			'capability_type'     => 'page',
 		);
 
-		register_post_type($this->post_type, $args);
+		register_post_type( $this->post_type, $args );
 
-		register_post_meta($this->post_type, 'type', [
-			'show_in_rest' => true,
-			'single' => true,
-			'auth_callback' => '__return_true',
-			'type' => 'string',
-			'description' => __('The layer type', 'jeo')
-		]);
+		register_post_meta(
+			$this->post_type,
+			'type',
+			array(
+				'show_in_rest'  => true,
+				'single'        => true,
+				'auth_callback' => '__return_true',
+				'type'          => 'string',
+				'description'   => __( 'The layer type', 'jeo' ),
+			)
+		);
 
-		register_post_meta($this->post_type, 'layer_type_options', [
-			'show_in_rest' => ['prepare_callback' => function($value) {return $value;}],
-			'single' => true,
-			'auth_callback' => '__return_true',
-			//'type' => 'string',
-			'description' => __('Layer type-specific options', 'jeo')
-		]);
+		register_post_meta(
+			$this->post_type,
+			'layer_type_options',
+			array(
+				'show_in_rest'  => array(
+					'schema' => array(
+						'properties'           => array(),
+						'additionalProperties' => true,
+					),
+				),
+				'single'        => true,
+				'auth_callback' => '__return_true',
+				'type'          => 'object',
+				'description'   => __( 'Layer type-specific options', 'jeo' ),
+			)
+		);
 	}
 
-	public function validate_meta_type($meta_value) {
-		var_dump($meta_value);
-		if ( ! \jeo_layer_types()->is_layer_type_registered($meta_value) ) {
+	public function validate_meta_type( $meta_value ) {
+		if ( ! \jeo_layer_types()->is_layer_type_registered( $meta_value ) ) {
 			return new \WP_Error( 'rest_invalid_field', __( 'Layer type not registered', 'jeo' ), array( 'status' => 400 ) );
 		}
 		return true;
