1e39dc6709fd4ade0bc394671f600ae3b73f3174
leogermani <leogermani@gmail.com>
2020-01-23 11:40:43 -0300

    add source URL to layers #28

--- src/templates/map-content-layers-list.php
+++ src/templates/map-content-layers-list.php
@@ -2,3 +2,8 @@
 <?php the_post_thumbnail(); ?>
 <?php the_content(); ?>
 <?php _e('Attribution:', 'jeo'); ?> <?php echo get_post_meta( get_the_ID(), 'attribution', true ); ?>
+<?php if ( $source_url ): ?>
+	<p>
+		<a href="<?php echo $source_url; ?>"><?php _e('Download source', 'jeo'); ?></a>
+	</p>
+<?php endif; ?>
