12a499c7c74304b850a21191a9bc1969316202ae
Capi Etheriel <barraponto@gmail.com>
2020-02-04 02:27:21 -0300

    touches map settings tab

--- src/js/src/map-blocks/map-settings.js
+++ src/js/src/map-blocks/map-settings.js
@@ -1,16 +1,20 @@
 import Form from 'react-jsonschema-form';
+import { __ } from '@wordpress/i18n';
 
 const mapSchema = {
 	type: 'object',
 	properties: {
-		center_lat: { type: 'number' },
-		center_lon: { type: 'number' },
-		initial_zoom: { type: 'number' },
+		center_lat: { title: __( 'Center Latitude' ), type: 'number' },
+		center_lon: { title: __( 'Center Longitude' ), type: 'number' },
+		initial_zoom: { title: __( 'Initial Zoom' ), type: 'number' },
+		min_zoom: { title: __( 'Min Zoom' ), type: 'number' },
+		max_zoom: { title: __( 'Max Zoom' ), type: 'number' },
 	},
 };
 
 export default ( { setMap } ) => (
 	<Form
+		className="jeo-map-settings"
 		schema={ mapSchema }
 		onSubmit={ ( { formData }, event ) => {
 			event.preventDefault();
