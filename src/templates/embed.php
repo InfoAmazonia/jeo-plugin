693876d2512f436d60a5d620cb0d69052dfbc3b4
leogermani <leogermani@gmail.com>
2020-02-11 17:26:15 -0300

    embed url basics #15

---
+++ src/templates/embed.php
@@ -0,0 +1,17 @@
+<html>
+
+	<head>
+		<title></title>
+		<?php wp_head(); ?>
+	</head>
+
+<body>
+
+<div class="jeomap" data-map_id="<?php echo $map_id; ?>" data-options='{"marker_action": "embed_preview"}' ></div>
+
+<div id="embed-post-preview"></div>
+
+</body>
+
+
+</html>
