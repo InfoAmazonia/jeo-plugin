38133630a250fa0d96d6b2bb7d482f1f9a812ddf
Leonardo <leopiccionia@gmail.com>
2020-02-12 17:14:20 -0300

    auto-fix formatting

--- src/js/src/posts-sidebar/index.js
+++ src/js/src/posts-sidebar/index.js
@@ -1,49 +1,45 @@
-import { registerPlugin } from "@wordpress/plugins";
-import { PluginDocumentSettingPanel } from "@wordpress/edit-post";
-import { Modal, Button } from "@wordpress/components";
-import JeoGeocodePosts from "./geo-posts";
-import { __ } from "@wordpress/i18n";
+import { registerPlugin } from '@wordpress/plugins';
+import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
+import { Modal, Button } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import JeoGeocodePosts from './geo-posts';
 
 const JeoGeocodePanel = class JeoGeocodePanel extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			isOpen: false
-		}
+			isOpen: false,
+		};
 	}
 	render() {
-		let isOpen = this.state.isOpen;
+		const isOpen = this.state.isOpen;
 		return (
 			<>
 				<Button isDefault onClick={ () => this.setState( { isOpen: true } ) }>Open Modal</Button>
 				{ isOpen && (
 					<Modal
-						title={__('Geolocate this post', 'jeo')}
+						title={ __( 'Geolocate this post', 'jeo' ) }
 						onRequestClose={ () => this.setState( { isOpen: false } ) }>
 						<JeoGeocodePosts
 							onSaveLocation={ () => this.setState( { isOpen: false } ) }
 							onCancel={ () => this.setState( { isOpen: false } ) }
-							/>
+						/>
 					</Modal>
 				) }
 			</>
 		);
 	}
-
-
 };
-
-
 
 registerPlugin( 'myprefix-sidebar', {
 	icon: 'location-alt',
 	render: () => {
 		return (
 			<PluginDocumentSettingPanel
-			title={__('Geolocate this post', 'jeo')}
+				title={ __( 'Geolocate this post', 'jeo' ) }
 			>
 				<JeoGeocodePanel />
 			</PluginDocumentSettingPanel>
-		)
-	}
-});
+		);
+	},
+} );
