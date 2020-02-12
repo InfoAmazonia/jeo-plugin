cb939249ab1bb4b2ad384eab0d4922ca7e61a84e
leogermani <leogermani@gmail.com>
2020-02-12 15:53:31 -0300

    fix margin for default theme #15

--- src/templates/embed.php
+++ src/templates/embed.php
@@ -1,11 +1,11 @@
-<html>
+<html style="margin: 0px !important;">
 
 	<head>
 		<title></title>
 		<?php wp_head(); ?>
 	</head>
 
-<body>
+<body style="margin: 0px !important;">
 
 <div id="embed-container" style="<?php echo $container_style; ?>">
 	<div class="jeomap" data-map_id="<?php echo $map_id; ?>" data-options='{"marker_action": "embed_preview"}' style="<?php echo $map_style; ?>"></div>
