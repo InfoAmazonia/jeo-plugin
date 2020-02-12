c113ea9690d4d9ea97bf3e8b24887201d7010b28
Leonardo <leopiccionia@gmail.com>
2020-02-12 15:52:12 -0300

    auto-fix formatting

--- src/js/src/posts-sidebar/geo-posts.js
+++ src/js/src/posts-sidebar/geo-posts.js
@@ -2,37 +2,36 @@ import React from 'react';
 import { Map as LeafletMap, Marker, TileLayer } from 'react-leaflet';
 import classNames from 'classnames';
 import JeoGeoAutoComplete from './geo-auto-complete';
-import { Button, RadioControl, TabPanel } from "@wordpress/components";
-import { __ } from "@wordpress/i18n";
+import { Button, RadioControl, TabPanel } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 import './geo-posts.css';
 
 class JeoGeocodePosts extends React.Component {
 	constructor() {
 		super();
-		const metadata = wp.data.select('core/editor').getCurrentPost().meta;
+		const metadata = wp.data.select( 'core/editor' ).getCurrentPost().meta;
 
 		this.state = {
 			zoom: 1,
 			points: metadata._related_point,
-			currentMarkerIndex: 0
-		}
+			currentMarkerIndex: 0,
+		};
 
-		this.onLocationFound = this.onLocationFound.bind(this);
-		this.onMarkerDragged = this.onMarkerDragged.bind(this);
-		this.getProperty = this.getProperty.bind(this);
-		this.save = this.save.bind(this);
-		this.clickMarkerMap = this.clickMarkerMap.bind(this);
-		this.clickMarkerList = this.clickMarkerList.bind(this);
-		this.mapLoaded = this.mapLoaded.bind(this);
-		this.relevanceClick = this.relevanceClick.bind(this);
-		this.updatePoint = this.updatePoint.bind(this);
-		this.updateCurrentPoint = this.updateCurrentPoint.bind(this);
-		this.newPoint = this.newPoint.bind(this);
-		this.deletePoint = this.deletePoint.bind(this);
+		this.onLocationFound = this.onLocationFound.bind( this );
+		this.onMarkerDragged = this.onMarkerDragged.bind( this );
+		this.getProperty = this.getProperty.bind( this );
+		this.save = this.save.bind( this );
+		this.clickMarkerMap = this.clickMarkerMap.bind( this );
+		this.clickMarkerList = this.clickMarkerList.bind( this );
+		this.mapLoaded = this.mapLoaded.bind( this );
+		this.relevanceClick = this.relevanceClick.bind( this );
+		this.updatePoint = this.updatePoint.bind( this );
+		this.updateCurrentPoint = this.updateCurrentPoint.bind( this );
+		this.newPoint = this.newPoint.bind( this );
+		this.deletePoint = this.deletePoint.bind( this );
 
 		this.refMap = React.createRef();
-
-	};
+	}
 
 	/**
 	 * Immutably updates info in the point object, inside the array of related points
@@ -52,7 +51,7 @@ class JeoGeocodePosts extends React.Component {
 		} );
 	}
 
-	updateCurrentPoint(data) {
+	updateCurrentPoint( data ) {
 		const marker_id = this.state.currentMarkerIndex;
 		this.updatePoint( marker_id, data );
 	}
@@ -69,133 +68,122 @@ class JeoGeocodePosts extends React.Component {
 		} );
 	}
 
-	relevanceClick(option) {
-		this.updateCurrentPoint( {relevance: option} );
+	relevanceClick( option ) {
+		this.updateCurrentPoint( { relevance: option } );
 	}
 
 	newPoint() {
-
 		const center = this.refMap.current.leafletElement.getCenter();
 
-		this.setState({
+		this.setState( {
 			...this.state,
 			points: [
 				...this.state.points,
 				{
 					_geocode_lat: center.lat,
 					_geocode_lon: center.lng,
-					_geocode_full_address: __('Unknown location', 'jeo')
-				}
+					_geocode_full_address: __( 'Unknown location', 'jeo' ),
+				},
 			],
-			currentMarkerIndex: this.state.points.length // sets new marker as current
-		});
+			currentMarkerIndex: this.state.points.length, // sets new marker as current
+		} );
 	}
 
-	deletePoint(e) {
+	deletePoint( e ) {
 		e.preventDefault();
 		e.stopPropagation();
 
 		const index = e.target.attributes.marker_index.value;
 
-		this.setState({
+		this.setState( {
 			...this.state,
-			points: this.state.points.filter( (el, i) => i != index ),
-			currentMarkerIndex: 0
-		});
+			points: this.state.points.filter( ( el, i ) => i != index ),
+			currentMarkerIndex: 0,
+		} );
 	}
 
-	getProperty(object, property) {
-		if ( undefined !== typeof(object[property]) ) {
-			return object[property];
-		} else {
-			return null;
+	getProperty( object, property ) {
+		if ( undefined !== typeof ( object[ property ] ) ) {
+			return object[ property ];
 		}
+		return null;
 	}
 
-	onLocationFound(location) {
-
+	onLocationFound( location ) {
 		this.updateCurrentPoint( {
-			_geocode_lat: this.getProperty(location, 'lat'),
-			_geocode_lon: this.getProperty(location, 'lon'),
-			_geocode_full_address: this.getProperty(location, 'full_address'),
-			_geocode_country: this.getProperty(location, 'country'),
-			_geocode_country_code: this.getProperty(location, 'country_code'),
-			_geocode_region_level_1: this.getProperty(location, 'region_level_1'),
-			_geocode_region_level_2: this.getProperty(location, 'region_level_2'),
-			_geocode_region_level_3: this.getProperty(location, 'region_level_3'),
-			_geocode_city: this.getProperty(location, 'city'),
-			_geocode_city_level_1: this.getProperty(location, 'city_level_1')
+			_geocode_lat: this.getProperty( location, 'lat' ),
+			_geocode_lon: this.getProperty( location, 'lon' ),
+			_geocode_full_address: this.getProperty( location, 'full_address' ),
+			_geocode_country: this.getProperty( location, 'country' ),
+			_geocode_country_code: this.getProperty( location, 'country_code' ),
+			_geocode_region_level_1: this.getProperty( location, 'region_level_1' ),
+			_geocode_region_level_2: this.getProperty( location, 'region_level_2' ),
+			_geocode_region_level_3: this.getProperty( location, 'region_level_3' ),
+			_geocode_city: this.getProperty( location, 'city' ),
+			_geocode_city_level_1: this.getProperty( location, 'city_level_1' ),
 		} );
 
-		this.refMap.current.leafletElement.flyTo([
-			this.getProperty(location, 'lat'),
-			this.getProperty(location, 'lon')
-		]);
+		this.refMap.current.leafletElement.flyTo( [
+			this.getProperty( location, 'lat' ),
+			this.getProperty( location, 'lon' ),
+		] );
+	}
 
-	};
-
-
-	mapLoaded(e) {
-
-		const coords = this.state.points.map( e => {
+	mapLoaded( e ) {
+		const coords = this.state.points.map( ( e ) => {
 			return [
-				parseFloat(e._geocode_lat),
-				parseFloat(e._geocode_lon)
-			]
+				parseFloat( e._geocode_lat ),
+				parseFloat( e._geocode_lon ),
+			];
 		} );
 
 		const map = e.target;
 
 		if ( coords.length == 1 ) {
-			map.setZoom(6);
-			map.panTo(coords[0]);
+			map.setZoom( 6 );
+			map.panTo( coords[ 0 ] );
 		} else if ( coords.length > 1 ) {
-			map.fitBounds(coords);
+			map.fitBounds( coords );
 		}
-
 	}
 
-	onMarkerDragged(e) {
+	onMarkerDragged( e ) {
 		const marker = e.target;
 		const latLng = marker.getLatLng();
 
 		this.updateCurrentPoint( {
 			_geocode_lat: latLng.lat,
-			_geocode_lon: latLng.lng
+			_geocode_lon: latLng.lng,
 		} );
 
-		fetch(jeo.ajax_url + '?action=jeo_reverse_geocode&lat=' + latLng.lat + '&lon=' + latLng.lng)
-			.then( response => {
+		fetch( jeo.ajax_url + '?action=jeo_reverse_geocode&lat=' + latLng.lat + '&lon=' + latLng.lng )
+			.then( ( response ) => {
 				return response.json();
 			} )
-			.then( result => {
-
+			.then( ( result ) => {
 				this.updateCurrentPoint( {
-					_geocode_full_address: this.getProperty(result, 'full_address'),
-					_geocode_country: this.getProperty(result, 'country'),
-					_geocode_country_code: this.getProperty(result, 'country_code'),
-					_geocode_region_level_1: this.getProperty(result, 'region_level_1'),
-					_geocode_region_level_2: this.getProperty(result, 'region_level_2'),
-					_geocode_region_level_3: this.getProperty(result, 'region_level_3'),
-					_geocode_city: this.getProperty(result, 'city'),
-					_geocode_city_level_1: this.getProperty(result, 'city_level_1')
+					_geocode_full_address: this.getProperty( result, 'full_address' ),
+					_geocode_country: this.getProperty( result, 'country' ),
+					_geocode_country_code: this.getProperty( result, 'country_code' ),
+					_geocode_region_level_1: this.getProperty( result, 'region_level_1' ),
+					_geocode_region_level_2: this.getProperty( result, 'region_level_2' ),
+					_geocode_region_level_3: this.getProperty( result, 'region_level_3' ),
+					_geocode_city: this.getProperty( result, 'city' ),
+					_geocode_city_level_1: this.getProperty( result, 'city_level_1' ),
 				} );
-
 			} );
-
 	}
 
 	save() {
-		wp.data.dispatch('core/editor').editPost({meta: {
-			_related_point: this.state.points.filter( (el, i) => {
-				console.log(el._geocode_full_address != __('Unknown location', 'jeo'));
-				return el._geocode_full_address != __('Unknown location', 'jeo');
-			})
+		wp.data.dispatch( 'core/editor' ).editPost( { meta: {
+			_related_point: this.state.points.filter( ( el, i ) => {
+				console.log( el._geocode_full_address != __( 'Unknown location', 'jeo' ) );
+				return el._geocode_full_address != __( 'Unknown location', 'jeo' );
+			} ),
 
-		}}).then(() => {
-			this.props.onSaveLocation()
-		});
-
+		} } ).then( () => {
+			this.props.onSaveLocation();
+		} );
 	}
 
 	render() {
@@ -230,11 +218,11 @@ class JeoGeocodePosts extends React.Component {
 															<li
 																id={ i }
 																onClick={ this.clickMarkerList }
-																className={ classNames([
+																className={ classNames( [
 																	'jeo-geocode-posts__post',
 																	point && point.relevance || 'primary',
-																	this.state.currentMarkerIndex == i && 'active'
-																]) }
+																	this.state.currentMarkerIndex == i && 'active',
+																] ) }
 															>
 																{ point._geocode_full_address }
 																{ ' ' }
@@ -293,15 +281,15 @@ class JeoGeocodePosts extends React.Component {
 							<h2>{ __( 'Search your location', 'jeo' ) }</h2>
 							<JeoGeoAutoComplete onSelect={ this.onLocationFound } />
 							<RadioControl
-									label={ __( 'Relevance', 'jeo' ) }
-									//help="The type of the current user"
-									selected={ this.state.points.length ? this.state.points[ this.state.currentMarkerIndex ].relevance || 'primary' : 'primary' }
-									options={ [
-										{ label: __( 'Primary', 'jeo' ), value: 'primary' },
-										{ label: __( 'Secondary', 'jeo' ), value: 'secondary' },
-									] }
-									onChange={ this.relevanceClick }
-									/>
+								label={ __( 'Relevance', 'jeo' ) }
+								//help="The type of the current user"
+								selected={ this.state.points.length ? this.state.points[ this.state.currentMarkerIndex ].relevance || 'primary' : 'primary' }
+								options={ [
+									{ label: __( 'Primary', 'jeo' ), value: 'primary' },
+									{ label: __( 'Secondary', 'jeo' ), value: 'secondary' },
+								] }
+								onChange={ this.relevanceClick }
+							/>
 						</div>
 					) /* this.state.points.length */ }
 				</div>
@@ -319,7 +307,6 @@ class JeoGeocodePosts extends React.Component {
 				</div>
 			</div>
 		);
-
 	}
 }
 
