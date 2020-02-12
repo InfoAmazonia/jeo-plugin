812ece788af0127a52da951159d0e8a2e60fe39b
leogermani <leogermani@gmail.com>
2020-02-12 15:51:50 -0300

    embed: add size support #15

--- src/templates/embed.php
+++ src/templates/embed.php
@@ -7,9 +7,11 @@
 
 <body>
 
-<div class="jeomap" data-map_id="<?php echo $map_id; ?>" data-options='{"marker_action": "embed_preview"}' ></div>
+<div id="embed-container" style="<?php echo $container_style; ?>">
+	<div class="jeomap" data-map_id="<?php echo $map_id; ?>" data-options='{"marker_action": "embed_preview"}' style="<?php echo $map_style; ?>"></div>
 
-<div id="embed-post-preview"></div>
+	<div id="embed-post-preview"></div>
+</div>
 
 </body>
 
