dda246939f3c6382cde4b013000a59805999f8b1
Leonardo <leopiccionia@gmail.com>
2020-02-12 16:22:23 -0300

    remove reactabular-table

--- src/js/src/map-blocks/layers-table.js
+++ src/js/src/map-blocks/layers-table.js
@@ -1,20 +1,9 @@
 import { Button } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import * as Table from 'reactabular-table';
 
-const layerColumns = [
-	{ property: 'type', header: { label: __( 'Layer Type' ) } },
-	{ property: 'title', header: { label: __( 'Layer Name' ) } },
-];
-
-const loadLayers = ( layers ) => ( settings ) =>
-	layers.find( ( l ) => l.id === settings.id );
-
-const layerToRow = ( layer ) => ( {
-	id: layer.id,
-	type: layer.meta.type,
-	title: layer.title.rendered,
-} );
+const loadLayers = ( layers ) => ( settings ) => {
+	return layers.find( ( l ) => l.id === settings.id );
+};
 
 export default ( {
 	loadedLayers,
@@ -31,15 +20,26 @@ export default ( {
 	const loadLayer = loadLayers( loadedLayers );
 
 	return (
-		<Table.Provider columns={ layerColumns } className={ className }>
-			<Table.Header />
-			<Table.Body
-				rows={ selectedLayers.map( loadLayer ).map( layerToRow ) }
-				rowKey="id"
-			/>
+		<table className={ className }>
+			<thead>
+				<tr>
+					<th>{ __( 'Layer Type', 'jeo' ) }</th>
+					<th>{ __( 'Layer Name', 'jeo' ) }</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				{ selectedLayers.map( loadLayer ).map( ( layer ) => (
+					<tr id={ layer.id } key={ layer.id }>
+						<td>{ layer.meta.type }</td>
+						<td>{ layer.title.rendered }</td>
+					</tr>
+				) ) }
+			</tbody>
+
 			<tfoot>
 				<tr>
-					<td colSpan={ layerColumns.length }>
+					<td colSpan="2">
 						{ ! ( loadedLayers && loadedLayers.length ) && (
 							<p className="empty">{ emptyMessage }</p>
 						) }
@@ -51,6 +51,6 @@ export default ( {
 					</td>
 				</tr>
 			</tfoot>
-		</Table.Provider>
+		</table>
 	);
 };
