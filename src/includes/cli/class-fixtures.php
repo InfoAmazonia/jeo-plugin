05a3f9822f8e6cd216701360706375c3ac49502e
leogermani <leogermani@gmail.com>
2020-02-19 13:36:30 -0300

    bootstrapping legends #10

--- src/includes/cli/class-fixtures.php
+++ src/includes/cli/class-fixtures.php
@@ -53,7 +53,20 @@ class Fixtures {
 								]
 							]
 						]
-					]
+					],
+					'legend_type' => 'barscale',
+					'legend_title' => 'Número de árvores',
+					'legend_type_options' => [
+						'left_label' => '0',
+						'right_label' => '1000',
+						'colors' => [
+							'#9fa498',
+							'#858b7d',
+							'#5f645a',
+							'#474a42',
+							'#313629',
+						]
+					],
 				]
 			],
 			[
@@ -66,7 +79,8 @@ class Fixtures {
 					'source_url' => 'http://domain.com/source.csv',
 					'layer_type_options' => [
 						'url' => 'https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg'
-					]
+					],
+					'legend_type' => 'none',
 				]
 			],
 			[
@@ -79,7 +93,8 @@ class Fixtures {
 					'source_url' => 'http://domain.com/source.csv',
 					'layer_type_options' => [
 						'url' => 'https://wri-tiles.s3.amazonaws.com/glad_prod/tiles/{z}/{x}/{y}.png'
-					]
+					],
+					'legend_type' => 'none',
 				]
 			],
 			[
@@ -92,7 +107,8 @@ class Fixtures {
 					'source_url' => 'http://domain.com/source.csv',
 					'layer_type_options' => [
 						'style_id' => 'infoamazonia/ck33yfty30o0s1dqpien3edi4'
-					]
+					],
+					'legend_type' => 'none',
 				]
 			],
 
---
+++ src/includes/legend-types/JeoLegend.js
@@ -0,0 +1,27 @@
+import JeoLegendTypes from './JeoLegendTypes'
+
+class JeoLegend {
+	constructor(legendTypeSlug, attributes) {
+		this.legendType = JeoLegendTypes.getLegendType(legendTypeSlug);
+		this.attributes = attributes;
+
+	}
+	render(map) {
+		return this.legendType.render(map, this.attributes);
+	}
+
+	get title() {
+		return this.attributes.title;
+	}
+
+	get layer_id() {
+		return this.attributes.layer_id;
+	}
+
+	get options() {
+		return this.attributes.legend_type_options;
+	}
+}
+
+window.JeoLegend = JeoLegend;
+export default JeoLegend;
