d81da0060f5f13ffd53dcd203851661173273dce
Leonardo <leopiccionia@gmail.com>
2020-02-20 13:38:44 -0300

    add block for embedding map from library

--- src/js/src/map-blocks/map-editor.js
+++ src/js/src/map-blocks/map-editor.js
@@ -1,82 +1,113 @@
+import { Dashicon, PanelBody, PanelRow, Spinner, TextControl } from '@wordpress/components';
 import { withSelect } from '@wordpress/data';
-import { Fragment, useState } from '@wordpress/element';
-import MapEditorModal from './map-editor-modal';
-import LayersLibrary from './layers-library';
-import LayersSettings from './layers-settings';
-import MapSettings from './map-settings';
-import LayersTable from './layers-table';
+import { Fragment, useCallback, useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import React from 'react';
+import Autosuggest from 'react-autosuggest';
 import './map-editor.css';
 
 const MapEditor = ( {
 	attributes,
 	setAttributes,
-	loadedLayers,
-	loadingLayers,
+	loadedMaps,
+	loadingMaps,
 } ) => {
-	const [ modal, setModal ] = useState( false );
-	const selectedLayers = attributes.layers;
+	const [ value, setValue ] = useState( '' );
+	const [ suggestions, setSuggestions ] = useState( [] );
+
+	const onSuggestionsFetchRequested = useCallback( _.debounce( ( { value: val } ) => {
+		const input = val.trim().toLowerCase();
+		if ( input.length === 0 ) {
+			setSuggestions( [] );
+		}
+		setSuggestions( loadedMaps.filter( ( map ) => {
+			return map.title.rendered.toLowerCase().includes( input ) || map.content.rendered.toLowerCase().includes( input );
+		} ) );
+	}, 500 ), [ loadedMaps, setSuggestions ] );
+
+	const onSuggestionsClearRequested = useCallback( () => setSuggestions( [] ), [ setSuggestions ] );
+
+	const onSuggestionSelected = useCallback( ( e, { suggestion } ) => {
+		setAttributes( { ...attributes, map_id: suggestion.id } );
+	}, [ attributes, setAttributes ] );
+
+	const getSuggestionValue = useCallback( ( map ) => map.title.rendered, [] );
+
+	const inputProps = {
+		placeholder: __( 'Type a map name', 'jeo' ),
+		value,
+		onChange: useCallback( ( e, { newValue } ) => setValue( newValue ), [ setValue ] ),
+	};
+
+	let currentMap = '';
+	if ( loadedMaps && attributes.map_id ) {
+		currentMap = loadedMaps.find( ( map ) => map.id == attributes.map_id );
+		if ( value === '' ) {
+			setValue( currentMap.title.rendered );
+		}
+	}
+
+	const setSize = useCallback( ( key ) => ( val ) => {
+		const numVal = Number( val );
+		if ( _.isFinite( numVal ) ) {
+			setAttributes( { ...attributes, [ key ]: numVal } );
+		}
+	}, [ attributes, setAttributes ] );
+
+	const { InspectorControls } = wp.editor;
 
 	return (
 		<Fragment>
-			{ modal && (
-				<MapEditorModal modal={ modal } setModal={ setModal }>
-					{ ( tab ) => {
-						switch ( tab.name ) {
-							case 'map':
-								return (
-									<MapSettings
-										setMap={ ( settings ) =>
-											setAttributes( { ...attributes, ...settings } )
-										}
-									/>
-								);
-							case 'layers':
-								return (
-									<LayersSettings
-										loadedLayers={ loadedLayers }
-										loadingLayers={ loadingLayers }
-										selected={ selectedLayers }
-										setLayers={ ( layers ) =>
-											setAttributes( { ...attributes, layers } )
-										}
-									/>
-								);
-							case 'library':
-								return (
-									<LayersLibrary
-										selected={ selectedLayers }
-										setLayers={ ( layers ) =>
-											setAttributes( { ...attributes, layers } )
-										}
-									/>
-								);
-						}
-					} }
-				</MapEditorModal>
+			{ currentMap && (
+				<Fragment>
+					<Dashicon icon="location-alt" />
+					{ ' ' }
+					{ currentMap.title.rendered }
+				</Fragment>
 			) }
-
-			<LayersTable
-				loadedLayers={ loadedLayers }
-				loadingLayers={ loadingLayers }
-				selectedLayers={ selectedLayers }
-				onButtonClick={ () => setModal( 'library' ) }
-			/>
+			{ loadingMaps ?
+				<Spinner /> :
+				<Autosuggest
+					suggestions={ suggestions }
+					onSuggestionsFetchRequested={ onSuggestionsFetchRequested }
+					onSuggestionsClearRequested={ onSuggestionsClearRequested }
+					onSuggestionSelected={ onSuggestionSelected }
+					getSuggestionValue={ getSuggestionValue }
+					renderSuggestion={ ( map ) => <span>{ map.title.rendered }</span> }
+					inputProps={ inputProps }
+				/>
+			}
+			<InspectorControls key={ 'inspector' }>
+				<PanelBody title={ __( 'Size' ) } initialOpen={ true }>
+					<PanelRow>
+						<TextControl
+							label={ __( 'Height' ) }
+							value={ attributes.height }
+							onChange={ setSize( 'height' ) }
+						/>
+					</PanelRow>
+					<PanelRow>
+						<TextControl
+							label={ __( 'Width' ) }
+							value={ attributes.width }
+							onChange={ setSize( 'width' ) }
+						/>
+					</PanelRow>
+				</PanelBody>
+			</InspectorControls>
 		</Fragment>
 	);
 };
 
-export default withSelect( ( select, { attributes } ) => {
-	const query = { include: attributes.layers.map( ( layer ) => layer.id ) };
+export default withSelect( ( select ) => {
 	return {
-		loadedLayers: select( 'core' ).getEntityRecords(
+		loadedMaps: select( 'core' ).getEntityRecords(
 			'postType',
-			'map-layer',
-			query
+			'map',
 		),
-		loadingLayers: select( 'core/data' ).isResolving( 'core', 'getEntityRecords', [
+		loadingMaps: select( 'core/data' ).isResolving( 'core', 'getEntityRecords', [
 			'postType',
-			'map-layer',
-			query,
+			'map',
 		] ),
 	};
 } )( MapEditor );
---
+++ src/js/src/map-blocks/onetime-map-editor.js
@@ -0,0 +1,82 @@
+import { withSelect } from '@wordpress/data';
+import { Fragment, useState } from '@wordpress/element';
+import MapEditorModal from './map-editor-modal';
+import LayersLibrary from './layers-library';
+import LayersSettings from './layers-settings';
+import MapSettings from './map-settings';
+import LayersTable from './layers-table';
+import './onetime-map-editor.css';
+
+const OnetimeMapEditor = ( {
+	attributes,
+	setAttributes,
+	loadedLayers,
+	loadingLayers,
+} ) => {
+	const [ modal, setModal ] = useState( false );
+	const selectedLayers = attributes.layers;
+
+	return (
+		<Fragment>
+			{ modal && (
+				<MapEditorModal modal={ modal } setModal={ setModal }>
+					{ ( tab ) => {
+						switch ( tab.name ) {
+							case 'map':
+								return (
+									<MapSettings
+										setMap={ ( settings ) =>
+											setAttributes( { ...attributes, ...settings } )
+										}
+									/>
+								);
+							case 'layers':
+								return (
+									<LayersSettings
+										loadedLayers={ loadedLayers }
+										loadingLayers={ loadingLayers }
+										selected={ selectedLayers }
+										setLayers={ ( layers ) =>
+											setAttributes( { ...attributes, layers } )
+										}
+									/>
+								);
+							case 'library':
+								return (
+									<LayersLibrary
+										selected={ selectedLayers }
+										setLayers={ ( layers ) =>
+											setAttributes( { ...attributes, layers } )
+										}
+									/>
+								);
+						}
+					} }
+				</MapEditorModal>
+			) }
+
+			<LayersTable
+				loadedLayers={ loadedLayers }
+				loadingLayers={ loadingLayers }
+				selectedLayers={ selectedLayers }
+				onButtonClick={ () => setModal( 'library' ) }
+			/>
+		</Fragment>
+	);
+};
+
+export default withSelect( ( select, { attributes } ) => {
+	const query = { include: attributes.layers.map( ( layer ) => layer.id ) };
+	return {
+		loadedLayers: select( 'core' ).getEntityRecords(
+			'postType',
+			'map-layer',
+			query
+		),
+		loadingLayers: select( 'core/data' ).isResolving( 'core', 'getEntityRecords', [
+			'postType',
+			'map-layer',
+			query,
+		] ),
+	};
+} )( OnetimeMapEditor );
