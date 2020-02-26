39d3ef6f3bd65eaa782fc729967fde3bea84afa6
Leonardo <leopiccionia@gmail.com>
2020-02-26 18:01:36 -0300

    remove redundant file

--- src/includes/posts-selector/class-posts-selector.php
+++ src/includes/posts-selector/class-posts-selector.php
@@ -8,6 +8,15 @@ class Posts_Selector {
 
 	protected function init() {
 		add_action('admin_init', [$this, 'load_assets']);
+		add_action('rest_post_query', [$this, 'rest_post_query'], 10, 2);
+	}
+
+	public function rest_post_query($args, $request) {
+		if ($request['meta_query']) {
+			$args['meta_query'] = $request['meta_query'];
+		}
+
+		return $args;
 	}
 
 	public function load_assets() {
