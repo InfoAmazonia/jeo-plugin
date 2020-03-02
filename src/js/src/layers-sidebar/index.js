a70fbc8b88d001e6968925f841b7210addde79ef
Leonardo <leopiccionia@gmail.com>
2020-03-02 17:25:12 -0300

    rename component

--- src/js/src/layers-sidebar/index.js
+++ src/js/src/layers-sidebar/index.js
@@ -5,7 +5,7 @@ import { Fragment } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { registerPlugin } from '@wordpress/plugins';
 
-import LayerEditor from './layer-editor';
+import LayerSettings from './layer-settings';
 
 domReady( () => {
 	const currentPostType = select( 'core/editor' ).getCurrentPostType();
@@ -17,7 +17,7 @@ domReady( () => {
 				return (
 					<Fragment>
 						<PluginDocumentSettingPanel title={ __( 'Settings' ) }>
-							<LayerEditor />
+							<LayerSettings />
 						</PluginDocumentSettingPanel>
 					</Fragment>
 				);
