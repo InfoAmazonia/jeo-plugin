810139f2c5ee20ba682debe3eba639831076c18b
Isaque Melo <isaquegbmelo2@gmail.com>
2020-03-02 19:24:34 -0300

    Add title TextControl

--- src/js/src/posts-sidebar/legends-editor/legend-editor.js
+++ src/js/src/posts-sidebar/legends-editor/legend-editor.js
@@ -1,21 +1,33 @@
 import React from 'react';
+import { Fragment } from '@wordpress/element';
+import { TextControl } from '@wordpress/components';
 import JeoLegend from '../../../../includes/legend-types/JeoLegend';
+import { __ } from '@wordpress/i18n';
 
 class LegendEditor extends React.Component {
 	constructor() {
 		super();
 		const metadata = wp.data.select( 'core/editor' ).getCurrentPost().meta;
 
-		const legendObject = new JeoLegend( metadata.legend_type, {
+		this.legendObject = new JeoLegend( metadata.legend_type, {
 			title: metadata.legend_title,
 			legend_type_options: metadata.legend_type_options,
 		} );
 
-		console.log( metadata, legendObject );
+		console.log(this.legendObject);
 	}
 
 	render() {
-		return '';
+		return (
+			<Fragment>
+				<TextControl
+					label={ __( 'Legend Title' ) }
+					value={ this.legendObject.title }
+				/>
+
+			</Fragment>
+
+		);
 	}
 }
 
