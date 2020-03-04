ce9d8a12bd1c3fbcde363ba7c48528a4a3581790
Isaque Melo <isaquegbmelo2@gmail.com>
2020-03-03 21:57:05 -0300

    Adds ColorPalette and color picker for color changing

--- src/js/src/posts-sidebar/legends-editor/editors/simplecolor.js
+++ src/js/src/posts-sidebar/legends-editor/editors/simplecolor.js
@@ -1,14 +1,78 @@
 import React from 'react';
-import { Fragment } from '@wordpress/element';
+import { ColorPicker, ColorPalette } from '@wordpress/components';
+import { Fragment, setState } from '@wordpress/element';
 
 class SimplecolorEditor extends React.Component {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			attributes: this.props.legendObject.attributes,
+			selectedColor: this.props.legendObject.attributes.legend_type_options.colors[ 0 ],
+		};
+	}
+
 	render() {
-		console.log(this.props.legendObject);
+		const colorsData = this.state.attributes.legend_type_options.colors.map( ( data, index ) => {
+			return { name: data.label, color: data.color };
+		} );
+
 		return (
 			<Fragment>
-				{ ' Simplecolor ' }
+				<ColorPalette
+					colors={ colorsData }
+					value={ this.state.selectedColor.color }
+					onChange={ ( color ) => this.setState( {
+						selectedColor: {
+							color,
+							name: colorsData.find( ( colorDataObj ) => {
+								return colorDataObj.color === color;
+							} ).name,
+						},
+					} )
+					}
+					disableCustomColors={ true }
+				/>
+
+				<SelectedColorOptions selectedColorData={ this.state.selectedColor }>
+
+				</SelectedColorOptions>
+
 			</Fragment>
 
+		);
+	}
+}
+
+class SelectedColorOptions extends React.Component {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			color: this.props.selectedColorData.color,
+			name: this.props.selectedColorData.name,
+		};
+	}
+
+	static getDerivedStateFromProps( nextProps ) {
+		console.log(nextProps.selectedColorData);
+		return {
+			color: nextProps.selectedColorData.color,
+			name: nextProps.selectedColorData.name,
+		};
+	}
+
+	render() {
+		//console.log(this.state.color);
+		console.log(this.state.color);
+		return (
+			<Fragment>
+				<ColorPicker
+					color={ this.state.color }
+					onChangeComplete={ ( color ) => 'setState( { color } )' }
+					disableAlpha
+				/>
+			</Fragment>
 		);
 	}
 }
