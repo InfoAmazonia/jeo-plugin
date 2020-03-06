7092bd2db8297949a2f90a995a159e2521457aba
Capi Etheriel <barraponto@gmail.com>
2020-03-06 01:16:12 -0300

    fixes linting issues

--- src/js/src/map-blocks/onetime-map-editor.js
+++ src/js/src/map-blocks/onetime-map-editor.js
@@ -11,6 +11,8 @@ import LayersPanel from './layers-panel';
 import { layerLoader } from './utils';
 import './onetime-map-editor.css';
 
+const { map_defaults: mapDefaults } = window.jeo_settings;
+
 const OnetimeMapEditor = ( {
 	attributes,
 	setAttributes,
@@ -37,7 +39,8 @@ const OnetimeMapEditor = ( {
 				<MapPanel
 					attributes={ attributes }
 					setModal={ setModal }
-					panel={ PanelBody } />
+					panel={ PanelBody }
+				/>
 				<LayersPanel
 					attributes={ attributes }
 					setModal={ setModal }
@@ -50,10 +53,10 @@ const OnetimeMapEditor = ( {
 			<div className="jeo-preview-area">
 				<Map
 					style="mapbox://styles/mapbox/streets-v11"
-					zoom={ [ attributes.initial_zoom || jeo_settings.map_defaults.zoom ] }
+					zoom={ [ attributes.initial_zoom || mapDefaults.zoom ] }
 					center={ [
-						attributes.center_lon || jeo_settings.map_defaults.lng,
-						attributes.center_lat || jeo_settings.map_defaults.lat
+						attributes.center_lon || mapDefaults.lng,
+						attributes.center_lat || mapDefaults.lat,
 					] }
 					containerStyle={ { height: '20vh' } }
 				/>
