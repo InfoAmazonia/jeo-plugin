373188323e1614bfa02d3aa9ccdb07bee9106de5
Leonardo <leopiccionia@gmail.com>
2020-03-12 18:52:03 -0300

    Interactions modal (WIP)

--- src/js/src/layers-sidebar/interaction-settings.js
+++ src/js/src/layers-sidebar/interaction-settings.js
@@ -1,11 +1,22 @@
+import { SelectControl } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+
 import './interaction-settings.css';
 
 export default function InteractionSettings( {
 	layer,
 } ) {
 	return (
-		<div>
-			{ layer.id }
+		<div className="jeo-interaction-settings">
+			<span>{ layer.id }</span>
+			<SelectControl
+				label={ __( 'Show on', 'jeo' ) }
+				options={ [
+					{ label: 'none', value: 'none' },
+					{ label: 'click', value: 'click' },
+					{ label: 'mouseover', value: 'hover' },
+				] }
+			/>
 		</div>
 	);
 }
