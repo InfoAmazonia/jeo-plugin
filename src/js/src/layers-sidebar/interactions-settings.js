6ce2dc2b497fb5628e6b5f62674d6132d2a621cd
Leonardo <leopiccionia@gmail.com>
2020-03-12 16:07:44 -0300

    Interactions modal (WIP)

---
+++ src/js/src/layers-sidebar/interactions-settings.js
@@ -0,0 +1,20 @@
+import { Modal } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+
+import InteractionSettings from './interaction-settings';
+import './interactions-settings.css';
+
+export default function InteractionsSettings( {
+	onCloseModal,
+	layers,
+} ) {
+	return (
+		<Modal title={ __( 'Interactions', 'jeo' ) } onRequestClose={ onCloseModal }>
+			{
+				layers.map( ( layer ) => (
+					<InteractionSettings key={ layer.id } layer={ layer }/>
+				) )
+			}
+		</Modal>
+	);
+}
