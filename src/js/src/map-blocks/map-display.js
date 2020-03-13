637feaf8956d0a7b5f5a21d774f6460b6270b56a
hockpond <gustavoc.reis@hotmail.com>
2020-03-13 17:21:38 -0300

    Add alignment buttons to JEO map and JEO One-time Map

--- src/js/src/map-blocks/map-display.js
+++ src/js/src/map-blocks/map-display.js
@@ -1,7 +1,9 @@
-export default ( { attributes } ) => {
+import classNames from 'classnames'
+
+export default ( { attributes, className } ) => {
 	return (
 		<div
-			className="jeomap"
+			className={ classNames( [ 'jeomap', className ] ) }
 			data-map_id={ attributes.map_id }
 			style={ {
 				height: attributes.height,
