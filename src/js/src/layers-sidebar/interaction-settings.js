0e3519fed86ddf16ee6e25296055ae0fb1994c04
Leonardo <leopiccionia@gmail.com>
2020-03-13 10:55:54 -0300

    Convert interactions settings to table

--- src/js/src/layers-sidebar/interaction-settings.js
+++ src/js/src/layers-sidebar/interaction-settings.js
@@ -1,4 +1,5 @@
 import { SelectControl } from '@wordpress/components';
+import { Fragment, useMemo } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 import './interaction-settings.css';
@@ -6,17 +7,34 @@ import './interaction-settings.css';
 export default function InteractionSettings( {
 	layer,
 } ) {
+	const titleOptions = useMemo( () => {
+		return Object.keys( layer.fields ).map( ( field ) => {
+			return { label: field, value: field };
+		} );
+	}, [ layer.fields ] );
+
 	return (
-		<div className="jeo-interaction-settings">
-			<span>{ layer.id }</span>
-			<SelectControl
-				label={ __( 'Show on', 'jeo' ) }
-				options={ [
-					{ label: 'none', value: 'none' },
-					{ label: 'click', value: 'click' },
-					{ label: 'mouseover', value: 'hover' },
-				] }
-			/>
-		</div>
+		<Fragment>
+			<tr className="jeo-interaction-settings">
+				<td>{ layer.id }</td>
+				<td>
+					<SelectControl
+						options={ [
+							{ label: __( 'Disabled', 'jeo' ), value: 'none' },
+							{ label: __( 'Show on click', 'jeo' ), value: 'click' },
+							{ label: __( 'Show on hover', 'jeo' ), value: 'mouseover' },
+						] }
+					/>
+				</td>
+				<td>
+					<SelectControl
+						options={ titleOptions }
+					/>
+				</td>
+			</tr>
+			<tr>
+				<td colSpan="3">TODO</td>
+			</tr>
+		</Fragment>
 	);
 }
--- src/js/src/layers-sidebar/interactions-settings.js
+++ src/js/src/layers-sidebar/interactions-settings.js
@@ -1,4 +1,5 @@
 import { Modal } from '@wordpress/components';
+import { useMemo } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 import InteractionSettings from './interaction-settings';
@@ -8,14 +9,32 @@ export default function InteractionsSettings( {
 	onCloseModal,
 	layers,
 } ) {
+	const interactiveLayers = useMemo( () => {
+		if ( ! layers ) {
+			return [];
+		}
+		return layers.filter( ( layer ) => {
+			return layer.fields && Object.keys( layer.fields ).length > 0;
+		} );
+	}, [ layers ] );
+
 	return (
-		<Modal title={ __( 'Interactions', 'jeo' ) } onRequestClose={ onCloseModal }>
-			{ layers.map( ( layer ) => (
-				<InteractionSettings
-					key={ layer.id }
-					layer={ layer }
-				/>
-			) ) }
+		<Modal className="jeo-interactions-settings__modal" title={ __( 'Interactions', 'jeo' ) } onRequestClose={ onCloseModal }>
+			<table className="jeo-interactions-settings">
+				{ ( interactiveLayers.length > 0 ) && (
+					<tr>
+						<th>{ __( 'Layer', 'jeo' ) }</th>
+						<th>{ __( 'Popup', 'jeo' ) }</th>
+						<th>{ __( 'Title' ) }</th>
+					</tr>
+				) }
+				{ interactiveLayers.map( ( layer ) => (
+					<InteractionSettings
+						key={ layer.id }
+						layer={ layer }
+					/>
+				) ) }
+			</table>
 		</Modal>
 	);
 }
