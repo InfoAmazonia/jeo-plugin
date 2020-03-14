1437605f4bbdb04a8d512b8bfcd1de36a3e7e543
Leonardo <leopiccionia@gmail.com>
2020-03-14 16:20:19 -0300

    Improve interactions layout

--- src/js/src/layers-sidebar/interaction-settings.js
+++ src/js/src/layers-sidebar/interaction-settings.js
@@ -1,4 +1,4 @@
-import { SelectControl } from '@wordpress/components';
+import { SelectControl, TextControl } from '@wordpress/components';
 import { Fragment, useMemo } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
@@ -8,16 +8,22 @@ export default function InteractionSettings( {
 	layer,
 } ) {
 	const titleOptions = useMemo( () => {
-		return Object.keys( layer.fields ).map( ( field ) => {
-			return { label: field, value: field };
-		} );
+		return [
+			{ label: __( 'None', 'jeo' ), value: undefined },
+			...Object.keys( layer.fields ).map( ( field ) => {
+				return { label: field, value: field };
+			} ),
+		];
 	}, [ layer.fields ] );
+
+	const [ [ firstField, firstPlaceholder ], ...otherFields ] = Object.entries( layer.fields );
+	const numFields = otherFields.length + 1;
 
 	return (
 		<Fragment>
-			<tr className="jeo-interaction-settings">
-				<td>{ layer.id }</td>
-				<td>
+			<tr className="jeo-interaction-header">
+				<td rowSpan={ numFields }>{ layer.id }</td>
+				<td rowSpan={ numFields }>
 					<SelectControl
 						options={ [
 							{ label: __( 'Disabled', 'jeo' ), value: 'none' },
@@ -26,15 +32,40 @@ export default function InteractionSettings( {
 						] }
 					/>
 				</td>
-				<td>
+				<td rowSpan={ numFields }>
 					<SelectControl
 						options={ titleOptions }
 					/>
 				</td>
+				<td>
+					<label htmlFor={ `i_${ layer.id }_${ firstField }` }>
+						{ firstField }
+					</label>
+				</td>
+				<td>
+					<TextControl
+						id={ `i_${ layer.id }_${ firstField }` }
+						placeholder={ firstPlaceholder }
+					/>
+				</td>
 			</tr>
-			<tr>
-				<td colSpan="3">TODO</td>
-			</tr>
+			{ otherFields.map( ( [ field, placeholder ] ) => {
+				const inputId = `i_${ layer.id }_${ field }`;
+
+				return (
+					<tr key={ inputId }>
+						<td>
+							<label htmlFor={ inputId }>{ field }</label>
+						</td>
+						<td>
+							<TextControl
+								id={ inputId }
+								placeholder={ placeholder }
+							/>
+						</td>
+					</tr>
+				);
+			} ) }
 		</Fragment>
 	);
 }
