4152030ba3e097ac0b8d32cda2cb83c9b9ab8233
Leonardo <leopiccionia@gmail.com>
2020-03-16 15:48:00 -0300

    Abstract portal creation logic

--- src/js/src/map-blocks/map.js
+++ src/js/src/map-blocks/map.js
@@ -1,5 +1,5 @@
 import ReactMapboxGl from 'react-mapbox-gl';
 
-const MapboxAPIKey = window.jeo_settings.mapbox_key;
+export const MapboxAPIKey = window.jeo_settings.mapbox_key;
 
 export default ReactMapboxGl( { accessToken: MapboxAPIKey } );
