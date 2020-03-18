af493cfc2bbd9efe5b539a6188468c016b55963f
hockpond <gustavoc.reis@hotmail.com>
2020-03-18 12:32:17 -0300

    Add a layer preview when edditing a layer

--- src/js/src/layers-sidebar/index.js
+++ src/js/src/layers-sidebar/index.js
@@ -1,30 +1,20 @@
 import { select } from '@wordpress/data';
 import domReady from '@wordpress/dom-ready';
-import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
-import { Fragment } from '@wordpress/element';
-import { __ } from '@wordpress/i18n';
 import { registerPlugin } from '@wordpress/plugins';
 
-import LayerSettings from './layer-settings';
-import LegendsEditor from '../posts-sidebar/legends-editor/legend-editor';
+import LayersSidebar from './layers-sidebar';
 
 domReady( () => {
 	const currentPostType = select( 'core/editor' ).getCurrentPostType();
 
 	if ( currentPostType === 'map-layer' ) {
+		jQuery( '.editor-post-title' ).after( '<div id="layer-preview" style="margin: 0 10%">' );
+
 		registerPlugin( 'jeo-layers-sidebar', {
 			icon: null,
 			render: () => {
 				return (
-					<Fragment>
-						<PluginDocumentSettingPanel name="layer-settings" title={ __( 'Settings' ) }>
-							<LayerSettings />
-						</PluginDocumentSettingPanel>
-
-						<PluginDocumentSettingPanel name="legend-settings" title={ __( 'Legend' ) }>
-							<LegendsEditor />
-						</PluginDocumentSettingPanel>
-					</Fragment>
+					<LayersSidebar />
 				);
 			},
 		} );
