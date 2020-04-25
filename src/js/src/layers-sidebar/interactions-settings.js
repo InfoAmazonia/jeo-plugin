45270260cad48559c6e4ec52435a061b1554fe7b
Icaro Oliveira <icarooliv2@gmail.com>
2020-04-24 23:29:38 -0300

    group interactions by source layer

--- src/js/src/layers-sidebar/interactions-settings.js
+++ src/js/src/layers-sidebar/interactions-settings.js
@@ -1,5 +1,5 @@
-import { Modal, Panel } from '@wordpress/components';
-import { useCallback, useMemo } from '@wordpress/element';
+import { Modal, Panel, SelectControl} from '@wordpress/components';
+import { useCallback, useMemo, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 import InteractionSettings from './interaction-settings';
@@ -37,28 +37,52 @@ export default function InteractionsSettings( {
 		setInteractions( interactions.filter( ( interaction ) => interaction.id !== interactionId ) );
 	}, [ interactions, setInteractions ] );
 
+	const layersSources = useMemo(()=>{
+		if( ! layers) {
+			return [];
+		}
+		const uniqueSources = Array.from( new Set( layers.map( layer => layer.source ) ) )
+			.map( source => {
+				return layers.find( layer => layer.source === source )
+		})
+		let selectors = uniqueSources.map( u => { return { value: u.source, label: u.sourceName } } );
+		if(selectors.length > 1){
+			selectors.push( { value: 'all', label: __('All', 'jeo') } );
+		}
+		return selectors;
+	}, [ layersSources ])
+
+	const [ selectedSource, setSelectedSource ] = useState( layersSources.length > 1 ? 'all': layersSources[0].source );
+
 	return (
 		<Modal
 			className="jeo-interactions-settings__modal"
 			title={ __( 'Interactions', 'jeo' ) }
 			onRequestClose={ onCloseModal }
 		>
+			<SelectControl
+				label="selectedSource"
+				value={ selectedSource }
+				options={ layersSources }
+				onChange={ ( selectedSource ) => { setSelectedSource( selectedSource ) } }
+			/>
 			<Panel className="jeo-interactions-settings">
 				{ interactiveLayers.map( ( layer ) => {
-					const index = interactions.findIndex( ( x ) => x.id === layer.id );
-					const interaction = interactions[ index ];
-
-					return (
-						<InteractionSettings
-							key={ layer.id }
-							interactionIndex={ index }
-							interaction={ interaction || defaultInteraction( layer.id ) }
-							layer={ layer }
-							onInsert={ onInsert }
-							onUpdate={ onUpdate }
-							onDelete={ onDelete }
-						/>
-					);
+					if (layer.source === selectedSource || selectedSource === 'all' || layersSources.length <= 1) {
+						const index = interactions.findIndex( ( x ) => x.id === layer.id );
+						const interaction = interactions[ index ];
+						return (
+							<InteractionSettings
+								key={ layer.id }
+								interactionIndex={ index }
+								interaction={ interaction || defaultInteraction( layer.id ) }
+								layer={ layer }
+								onInsert={ onInsert }
+								onUpdate={ onUpdate }
+								onDelete={ onDelete }
+							/>
+						);
+					}
 				} ) }
 			</Panel>
 		</Modal>
