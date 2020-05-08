c55e49622ba68fe09114014658a4509aaa0162a9
Icaro Oliveira <icarooliv2@gmail.com>
2020-05-08 10:59:49 -0300

    (wip) fix labels

--- src/templates/map-content-layers-list.php
+++ src/templates/map-content-layers-list.php
@@ -4,6 +4,6 @@
 <?php _e('Attribution: ', 'jeo'); ?><a href="<?php echo esc_url( get_post_meta( get_the_ID(), 'attribution', true ) ); ?>"><?php echo esc_html( get_post_meta( get_the_ID(), 'attribution', true ) ); ?></a>
 <?php if ( $source_url ): ?>
 	<p>
-		<a href="<?php echo esc_url( $source_url );?>" class="download-source"><?php _e('Download source', 'jeo'); ?></a>
+		<a href="<?php echo esc_url( $source_url );?>" class="download-source"><?php _e('Download from source', 'jeo'); ?></a>
 	</p>
 <?php endif; ?>
