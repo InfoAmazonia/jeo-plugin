0fc54da8756611c970e2813de5dae54764c14f7d
pamelars86 <pamela.rosales@pucp.pe>
2020-05-12 17:36:32 -0300

    css for layers link in layer wrapper in map

--- src/js/src/jeo-map/index.js
+++ src/js/src/jeo-map/index.js
@@ -17,7 +17,6 @@ class JeoMap {
 		const map = new window.mapboxgl.Map( {
 			container: element,
 			attributionControl: false,
-		} );
 
 		this.map = map;
 		this.options = jQuery( this.element ).data( 'options' );
