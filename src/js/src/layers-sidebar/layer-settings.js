53dfb5cd6c911ed38908c1d4992d34c2230bf693
Icaro Oliveira <icarooliv2@gmail.com>
2020-05-29 21:20:51 -0300

    refactor layer settings

--- src/js/src/layers-sidebar/layer-settings.js
+++ src/js/src/layers-sidebar/layer-settings.js
@@ -9,9 +9,6 @@ import InteractionsSettings from './interactions-settings';
 const layerSchema = {
 	type: 'object',
 	properties: {
-		source_url: { title: __( 'Source data download link' ), type: 'string' },
-		attribution: { title: __( 'Data attribution link' ), type: 'string' },
-		attribution_name: { title: __( 'Source name' ), type: 'string' },
 		type: { title: __( 'Type' ), type: 'string' },
 	},
 	required: [ 'type' ],
@@ -36,19 +33,35 @@ const LayerSettings = ( {
 	const [ widgets, setWidgets ] = useState( {} );
 	const [ options, setOptions ] = useState( {} );
 	const [ styleLayers, setStyleLayers ] = useState( null );
+
 	const [ modalOpen, setModalStatus ] = useState( false );
+	const closeModal = useCallback( () => setModalStatus( false ), [ setModalStatus ] );
+	const openModal = useCallback( () => setModalStatus( true ), [ setModalStatus ] );
 
 	layerSchema.properties.type.enum = window.JeoLayerTypes.getLayerTypes();
 	layerSchema.properties.layer_type_options = options;
+
+	const interactions = useMemo( () => {
+		return postMeta.layer_type_options.interactions || [];
+	}, [ postMeta.layer_type_options.interactions ] );
+
+	const setInteractions = useCallback( ( e ) => {
+		setPostMeta( {
+			...postMeta,
+			layer_type_options: {
+				...postMeta.layer_type_options,
+				interactions: e,
+			},
+		} );
+	}, [ postMeta, setPostMeta ] );
 
 	useEffect( () => {
 		if ( postMeta.type ) {
 			window.JeoLayerTypes
 				.getLayerTypeSchema( postMeta )
-				.then( formUpdater( setOptions, setWidgets ) );
+				.then( formUpdater( setOptions, setWidgets, postMeta, setPostMeta ) );
 		} else {
 			setOptions( {} );
-			setWidgets( {} ); // technically unnecessary
 		}
 	}, [ postMeta.type ] );
 
@@ -61,22 +74,6 @@ const LayerSettings = ( {
 		}
 	}, [ postMeta, setStyleLayers ] );
 
-	const closeModal = useCallback( () => setModalStatus( false ), [ setModalStatus ] );
-	const openModal = useCallback( () => setModalStatus( true ), [ setModalStatus ] );
-
-	const interactions = useMemo( () => {
-		return postMeta.layer_type_options.interactions || [];
-	}, [ postMeta.layer_type_options.interactions ] );
-	const setInteractions = useCallback( ( e ) => {
-		setPostMeta( {
-			...postMeta,
-			layer_type_options: {
-				...postMeta.layer_type_options,
-				interactions: e,
-			},
-		} );
-	}, [ postMeta, setPostMeta ] );
-
 	return (
 		<Fragment>
 			<Form
@@ -85,39 +82,6 @@ const LayerSettings = ( {
 				uiSchema={ widgets }
 				formData={ postMeta }
 				onChange={ ( { formData } ) => {
-					const regex = new RegExp( /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi );
-
-					let attributionLink = formData.attribution;
-					let sourceLink = formData.source_url;
-
-					if ( formData.attribution && ! formData.attribution.includes( 'http' ) ) {
-						if ( formData.attribution.match( regex ) ) {
-							attributionLink = `https://${ formData.attribution }`;
-						} else if ( formData.attribution[ 0 ] !== '/' ) {
-							attributionLink = '/' + attributionLink;
-						}
-					}
-					formData.attribution = attributionLink;
-
-					if ( formData.source_url && ! formData.source_url.includes( 'http' ) ) {
-						if ( formData.source_url.match( regex ) ) {
-							sourceLink = `https://${ formData.source_url }`;
-						} else if ( formData.source_url[ 0 ] !== '/' ) {
-							sourceLink = '/' + sourceLink;
-						}
-					}
-					formData.source_url = sourceLink;
-
-					if ( ! formData.attribution_name ) {
-						formData.attribution_name = '';
-					}
-					if ( ! formData.attribution ) {
-						formData.attribution = '';
-					}
-
-					if ( ! formData.source_url ) {
-						formData.source_url = '';
-					}
 					setPostMeta( formData );
 				} }
 			>
