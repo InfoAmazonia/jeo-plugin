ab1c75216e17815a58fc1b5a83f00354c66725e1
Icaro Oliveira <icarooliv2@gmail.com>
2020-06-18 12:47:04 -0300

    hide attribution label if attribution is not defined [refs #118]

--- src/templates/map-content-layers-list.php
+++ src/templates/map-content-layers-list.php
@@ -1,10 +1,12 @@
 <h3><?php the_title(); ?></h3>
 <?php the_post_thumbnail(); ?>
 <?php the_content(); ?>
-<?php if(strlen($attribution_name) > 0): ?>
-	<?php _e('Attribution: ', 'jeo'); ?><a href="<?php echo $attribution; ?>"><?php echo esc_html( $attribution_name ); ?></a>
-<? else: ?>
-	<?php _e('Attribution: ', 'jeo'); ?><a href="<?php echo $attribution; ?>"><?php echo esc_html( $attribution ); ?></a>
+<?php if(strlen($attribution) > 0): ?>
+	<?php if(strlen($attribution_name) > 0): ?>
+		<?php _e('Attribution: ', 'jeo'); ?><a href="<?php echo $attribution; ?>"><?php echo esc_html( $attribution_name ); ?></a>
+	<? else: ?>
+		<?php _e('Attribution: ', 'jeo'); ?><a href="<?php echo $attribution; ?>"><?php echo esc_html( $attribution ); ?></a>
+	<?php endif; ?>
 <?php endif; ?>
 <?php if ( $source_url ): ?>
 	<p>
