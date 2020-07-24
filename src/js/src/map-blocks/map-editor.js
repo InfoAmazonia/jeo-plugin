f9d528e91cac115e3eac8b8ae03e64f7df8307fa
Icaro Oliveira <icarooliv2@gmail.com>
2020-07-24 13:32:55 -0300

    fix error prone queries at one-time-map

--- src/js/src/map-blocks/map-editor.js
+++ src/js/src/map-blocks/map-editor.js
@@ -154,7 +154,7 @@ const applyWithSelect = withSelect( ( select, { attributes } ) => ( {
 			'map',
 			attributes.map_id,
 		] ),
-	loadedLayers: select( 'core' ).getEntityRecords( 'postType', 'map-layer' ),
+	loadedLayers: select( 'core' ).getEntityRecords( 'postType', 'map-layer', { per_page: 100, order: 'asc', orderby: 'menu_order' } ),
 	loadingLayers: select( 'core/data' ).isResolving(
 		'core',
 		'getEntityRecords',
