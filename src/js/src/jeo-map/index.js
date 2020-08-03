cc21da47d9ec31d652e524075c712963395a55e5
Icaro Oliveira <icarooliv2@gmail.com>
2020-08-03 17:19:49 -0300

    verifies if map_id was found before assign it

--- src/js/src/jeo-map/index.js
+++ src/js/src/jeo-map/index.js
@@ -758,15 +758,18 @@ class JeoMap {
 				getting map_id as a class and creating it as an data element
 				check /src/js/src/map-blocks/map-display.js
 			*/
-
-			var map_id;
+			let map_id;
 			this.classList.forEach( ( cls ) => {
 				if ( cls.indexOf( 'map_id_' ) === 0 ) {
 					map_id = cls.substr( 7 );
 				}
 			} );
-			// creating data-map_id 
-			this.dataset['map_id'] = map_id;
+			// creating data-map_id if class exists
+			// otherwise this is a onetime map
+			// check initMap() method at this file
+			if ( map_id ) {
+				this.dataset['map_id'] = map_id;
+			}
 			new JeoMap( this );
 		} );
 	} );
