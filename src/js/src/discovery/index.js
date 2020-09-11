6d3118e7dd1340900b69b8c131aef0c6e515e70e
Isaque Melo <isaquegbmelo2@gmail.com>
2020-09-11 12:53:03 -0300

    Update JS

--- src/js/src/discovery/index.js
+++ src/js/src/discovery/index.js
@@ -3,11 +3,17 @@ import { Component } from '@wordpress/element';
 // mapboxgl.accessToken = 'MAPBOX_ACCESS_TOKEN';
 
 class Discorevy extends Component {
-	constructor(props) {
-		super(props);
+	constructor( props ) {
+		super( props );
+
 		this.state = {
+			// map
 			map: null,
-			itens: [],
+
+			// stories
+			storiesLoaded: false,
+			stories: [],
+
 		};
 	}
 
@@ -19,26 +25,38 @@ class Discorevy extends Component {
 			// zoom: this.state.zoom
 		} );
 
-		// this.setState({
-		// 	map,
-		// });
+		this.setState( {
+			map,
+		} );
 
 		// map.on('move', () => {
 		// 	console.log( this.state.map );
 		// });
 
-		this.fetchStories();
+		this.fetchStories().then( ( stories ) => {
+			stories.forEach( ( story ) => {
+				const storyPoints = story.meta._related_point;
+
+				if ( storyPoints.length ) {
+					storyPoints.forEach( ( point ) => {
+						this.addMarkerToMap( point );
+					} );
+				}
+			} );
+		} );
 	}
 
 	fetchStories() {
-		fetch( jeoMapVars.jsonUrl + 'posts' )
+		return fetch( jeoMapVars.jsonUrl + 'posts' )
 			.then( ( response ) => response.json() )
 			.then(
 				( stories ) => {
 					this.setState( {
 						storiesLoaded: true,
-						items: stories,
+						stories,
 					} );
+
+					return Promise.resolve( stories );
 				},
 				( error ) => {
 					console.log( error );
@@ -50,54 +68,46 @@ class Discorevy extends Component {
 			);
 	}
 
-	// addPostToMap(post) {
-	// 	if (post.meta._related_point) {
-	// 		post.meta._related_point.forEach((point) => {
-	// 			this.addPointToMap(point, post);
-	// 		});
-	// 	}
-	// }
+	addMarkerToMap( point ) {
+		const color = point.relevance === 'secondary' ? '#CCCCCC' : '#3FB1CE';
+		const marker = new mapboxgl.Marker( { color } );
 
-	// addPointToMap(point, post) {
-	// 	const color = point.relevance === 'secondary' ? '#CCCCCC' : '#3FB1CE';
-	// 	const marker = new mapboxgl.Marker({ color });
-	// 	marker.getElement().classList.add('marker');
+		// marker.getElement().classList.add( 'marker' );
+		// const popupHTML = this.popupTemplate( {
+		// 	post,
+		// 	read_more: window.jeoMapVars.string_read_more,
+		// 	show_featured_media: false,
+		// } );
 
-	// 	const popupHTML = this.popupTemplate({
-	// 		post,
-	// 		read_more: window.jeoMapVars.string_read_more,
-	// 		show_featured_media: false,
-	// 	});
+		//const popUp = new mapboxgl.Popup().setHTML(popupHTML);
 
-	// 	const popUp = new mapboxgl.Popup().setHTML(popupHTML);
+		const LngLat = {
+			lat: parseFloat( point._geocode_lat ),
+			lon: parseFloat( point._geocode_lon ),
+		};
 
-	// 	const LngLat = {
-	// 		lat: parseFloat(point._geocode_lat),
-	// 		lon: parseFloat(point._geocode_lon),
-	// 	};
+		marker.setLngLat( LngLat );
+		marker.addTo( this.state.map );
 
-	// 	marker.setLngLat(LngLat);
-	// 	marker.addTo(this.map);
-	// 	this.markers.push(marker);
+		// marker.getElement().addEventListener( 'click', () => {
+		// 	this.activateMarker(marker);
+		// 	if (
+		// 		!this.options ||
+		// 		!this.options.marker_action === 'embed_preview'
+		// 	) {
+		// 		marker.setPopup(popUp);
+		// 	} else {
+		// 		this.embedPreviewActive = true;
+		// 		this.updateEmbedPreview(post);
+		// 	}
+		// 	this.map.flyTo({ center: LngLat });
+		// });
 
-	// 	marker.getElement().addEventListener('click', () => {
-	// 		this.activateMarker(marker);
-	// 		if (
-	// 			!this.options ||
-	// 			!this.options.marker_action === 'embed_preview'
-	// 		) {
-	// 			marker.setPopup(popUp);
-	// 		} else {
-	// 			this.embedPreviewActive = true;
-	// 			this.updateEmbedPreview(post);
-	// 		}
-	// 		this.map.flyTo({ center: LngLat });
-	// 	});
+		// By default, fly to the first post and centers it
+		// this.activateMarker(marker);
 
-	// 	// By default, fly to the first post and centers it
-	// 	this.activateMarker(marker);
-	// 	this.map.flyTo({ center: LngLat, zoom: 4 });
-	// }
+		this.state.map.flyTo( { center: LngLat, zoom: 4 } );
+	}
 
 	render() {
 		return (
