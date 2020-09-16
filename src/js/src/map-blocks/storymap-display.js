2cc60d3bfbcea30a7aac24ef267553f24c5cc44e
hockpond <gustavoc.reis@hotmail.com>
2020-09-16 14:05:56 -0300

    Frontend test - storymap

--- src/js/src/map-blocks/storymap-display.js
+++ src/js/src/map-blocks/storymap-display.js
@@ -1,5 +1,35 @@
-export default ( { attributes, className } ) => {
-	return (
-		<h1>STORYMAP DISPLAY</h1>
-	);
-};
+import React, { Component } from 'react';
+import Map from './map';
+
+class StoryMapDisplay extends Component {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			currentChapter: this.props.attributes.slides[ 0 ],
+		}
+	}
+
+    render() {
+		return(
+			<Map
+				onStyleLoad={ ( map ) => {
+					map.scrollZoom.disable();
+					map.dragPan.disable();
+					map.touchZoomRotate.disable();
+					map.dragRotate.disable();
+				} }
+				style="mapbox://styles/mapbox/streets-v11"
+				zoom={ [ this.state.currentChapter.zoom ] }
+				center={ [
+					this.state.currentChapter.longitude,
+					this.state.currentChapter.latitude,
+				] }
+				containerStyle={ { height: '50vh' } }
+			>
+			</Map>
+		)
+	}
+}
+
+export default StoryMapDisplay;
