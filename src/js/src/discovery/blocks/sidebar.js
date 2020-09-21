1e140831118f19f08c8d4616e5165cf6e55dec54
Isaque Melo <isaquegbmelo2@gmail.com>
2020-09-21 11:24:59 -0300

    Discovery inifinit scrolling

--- src/js/src/discovery/blocks/sidebar.js
+++ src/js/src/discovery/blocks/sidebar.js
@@ -6,81 +6,89 @@ import MapLayers from './map-layers';
 class Sidebar extends Component {
 	constructor( props ) {
 		super( props );
+		this.handleScroll = this.handleScroll.bind(this);
 
-		let i;
 	}
 
 	displayTab(tab) {
-		return tab.name === 'stories'? <Stories /> : <MapLayers />;
+		return tab.name === 'stories'? <Stories stories={ this.props.stories } updateStories={ this.props.updateStories } /> : <MapLayers />;
 	}
+
+	handleScroll(e) {
+		const element = e.target
+		if (element.scrollHeight - element.scrollTop === element.clientHeight) {
+			this.props.updateStories({ cumulative: true })
+		}
+	};
 
 	render() {
 		return (
-			<TabPanel
-				className="togable-panel"
-				activeClass="active-tab"
-				// onSelect={ }
-				tabs={ [
-					{
-						name: 'stories',
-						title: (
-							<div className="tab-toggle">
-								<Icon
-									icon={ () => (
-										<svg
-											aria-hidden="true"
-											focusable="false"
-											data-prefix="far"
-											data-icon="newspaper"
-											role="img"
-											xmlns="http://www.w3.org/2000/svg"
-											viewBox="0 0 576 512">
-											<path
-												fill="currentColor"
-												d="M552 64H112c-20.858 0-38.643 13.377-45.248 32H24c-13.255 0-24 10.745-24 24v272c0 30.928 25.072 56 56 56h496c13.255 0 24-10.745 24-24V88c0-13.255-10.745-24-24-24zM48 392V144h16v248c0 4.411-3.589 8-8 8s-8-3.589-8-8zm480 8H111.422c.374-2.614.578-5.283.578-8V112h416v288zM172 280h136c6.627 0 12-5.373 12-12v-96c0-6.627-5.373-12-12-12H172c-6.627 0-12 5.373-12 12v96c0 6.627 5.373 12 12 12zm28-80h80v40h-80v-40zm-40 140v-24c0-6.627 5.373-12 12-12h136c6.627 0 12 5.373 12 12v24c0 6.627-5.373 12-12 12H172c-6.627 0-12-5.373-12-12zm192 0v-24c0-6.627 5.373-12 12-12h104c6.627 0 12 5.373 12 12v24c0 6.627-5.373 12-12 12H364c-6.627 0-12-5.373-12-12zm0-144v-24c0-6.627 5.373-12 12-12h104c6.627 0 12 5.373 12 12v24c0 6.627-5.373 12-12 12H364c-6.627 0-12-5.373-12-12zm0 72v-24c0-6.627 5.373-12 12-12h104c6.627 0 12 5.373 12 12v24c0 6.627-5.373 12-12 12H364c-6.627 0-12-5.373-12-12z"
-											></path>
-										</svg>
-									) }
-								/>
+			<div onScroll={ this.handleScroll }>
+				<TabPanel
+					className="togable-panel"
+					activeClass="active-tab"
+					// onSelect={ }
+					tabs={ [
+						{
+							name: 'stories',
+							title: (
+								<div className="tab-toggle">
+									<Icon
+										icon={ () => (
+											<svg
+												aria-hidden="true"
+												focusable="false"
+												data-prefix="far"
+												data-icon="newspaper"
+												role="img"
+												xmlns="http://www.w3.org/2000/svg"
+												viewBox="0 0 576 512">
+												<path
+													fill="currentColor"
+													d="M552 64H112c-20.858 0-38.643 13.377-45.248 32H24c-13.255 0-24 10.745-24 24v272c0 30.928 25.072 56 56 56h496c13.255 0 24-10.745 24-24V88c0-13.255-10.745-24-24-24zM48 392V144h16v248c0 4.411-3.589 8-8 8s-8-3.589-8-8zm480 8H111.422c.374-2.614.578-5.283.578-8V112h416v288zM172 280h136c6.627 0 12-5.373 12-12v-96c0-6.627-5.373-12-12-12H172c-6.627 0-12 5.373-12 12v96c0 6.627 5.373 12 12 12zm28-80h80v40h-80v-40zm-40 140v-24c0-6.627 5.373-12 12-12h136c6.627 0 12 5.373 12 12v24c0 6.627-5.373 12-12 12H172c-6.627 0-12-5.373-12-12zm192 0v-24c0-6.627 5.373-12 12-12h104c6.627 0 12 5.373 12 12v24c0 6.627-5.373 12-12 12H364c-6.627 0-12-5.373-12-12zm0-144v-24c0-6.627 5.373-12 12-12h104c6.627 0 12 5.373 12 12v24c0 6.627-5.373 12-12 12H364c-6.627 0-12-5.373-12-12zm0 72v-24c0-6.627 5.373-12 12-12h104c6.627 0 12 5.373 12 12v24c0 6.627-5.373 12-12 12H364c-6.627 0-12-5.373-12-12z"
+												></path>
+											</svg>
+										) }
+									/>
 
-								<span>Stories</span>
-							</div>
-						),
-						className: 'stories-tab',
-					},
-					{
-						name: 'mapLayers',
-						title: (
-							<div className="tab-toggle">
-								<Icon
-									icon={ () => (
-										<svg
-											aria-hidden="true"
-											focusable="false"
-											data-prefix="far"
-											data-icon="map"
-											role="img"
-											xmlns="http://www.w3.org/2000/svg"
-											viewBox="0 0 576 512"
-											class="svg-inline--fa fa-map fa-w-18 fa-3x"
-										>
-											<path
-												fill="currentColor"
-												d="M560.02 32c-1.96 0-3.98.37-5.96 1.16L384.01 96H384L212 35.28A64.252 64.252 0 0 0 191.76 32c-6.69 0-13.37 1.05-19.81 3.14L20.12 87.95A32.006 32.006 0 0 0 0 117.66v346.32C0 473.17 7.53 480 15.99 480c1.96 0 3.97-.37 5.96-1.16L192 416l172 60.71a63.98 63.98 0 0 0 40.05.15l151.83-52.81A31.996 31.996 0 0 0 576 394.34V48.02c0-9.19-7.53-16.02-15.98-16.02zM224 90.42l128 45.19v285.97l-128-45.19V90.42zM48 418.05V129.07l128-44.53v286.2l-.64.23L48 418.05zm480-35.13l-128 44.53V141.26l.64-.24L528 93.95v288.97z"
-											></path>
-										</svg>
-									) }
-								/>
+									<span>Stories</span>
+								</div>
+							),
+							className: 'stories-tab',
+						},
+						{
+							name: 'mapLayers',
+							title: (
+								<div className="tab-toggle">
+									<Icon
+										icon={ () => (
+											<svg
+												aria-hidden="true"
+												focusable="false"
+												data-prefix="far"
+												data-icon="map"
+												role="img"
+												xmlns="http://www.w3.org/2000/svg"
+												viewBox="0 0 576 512"
+											>
+												<path
+													fill="currentColor"
+													d="M560.02 32c-1.96 0-3.98.37-5.96 1.16L384.01 96H384L212 35.28A64.252 64.252 0 0 0 191.76 32c-6.69 0-13.37 1.05-19.81 3.14L20.12 87.95A32.006 32.006 0 0 0 0 117.66v346.32C0 473.17 7.53 480 15.99 480c1.96 0 3.97-.37 5.96-1.16L192 416l172 60.71a63.98 63.98 0 0 0 40.05.15l151.83-52.81A31.996 31.996 0 0 0 576 394.34V48.02c0-9.19-7.53-16.02-15.98-16.02zM224 90.42l128 45.19v285.97l-128-45.19V90.42zM48 418.05V129.07l128-44.53v286.2l-.64.23L48 418.05zm480-35.13l-128 44.53V141.26l.64-.24L528 93.95v288.97z"
+												></path>
+											</svg>
+										) }
+									/>
 
-								<span>Map layers</span>
-							</div>
-						),
-						className: 'stories-tab',
-					},
-				] }
-			>
-				{ ( tab ) => this.displayTab( tab ) }
-			</TabPanel>
+									<span>Map layers</span>
+								</div>
+							),
+							className: 'stories-tab',
+						},
+					] }
+				>
+					{ ( tab ) => this.displayTab( tab ) }
+				</TabPanel>
+			</div>
 		);
 	}
 }
--- src/js/src/discovery/index.js
+++ src/js/src/discovery/index.js
@@ -2,172 +2,211 @@ import { Component } from '@wordpress/element';
 import Sidebar from './blocks/sidebar';
 import './style/discovery.scss';
 
-// mapboxgl.accessToken = 'MAPBOX_ACCESS_TOKEN';
+const POSTS_PER_PAGE = 4;
 
-class Discorevy extends Component {
+class Discovery extends Component {
 	constructor(props) {
 		super(props);
+		// map can't be a state, trust me.
+		this.map = null;
 
 		this.state = {
-			// map
-			map: null,
+			// component
+			firstLoad: true,
+			currentPage: 1,
+			totalPages: null,
+			totalPosts: null,
 
 			// layers
 			selectedLayers: [],
+
 			// stories
 			storiesLoaded: false,
 			stories: [],
 		};
+
+		this.updateStories = this.updateStories.bind(this);
+
 	}
 
 	componentDidMount() {
 		const map = new mapboxgl.Map({
 			container: this.mapContainer,
 			style: 'mapbox://styles/mapbox/streets-v11',
-			// style: 'mapbox://styles/mapbox/light-v9',
 			// center: [this.state.lng, this.state.lat],
 			// zoom: this.state.zoom
 		});
 
-		this.setState({
-			map,
-		});
+		this.map = map;
 
-		this.fetchStories().then((stories) => {
-			const sourceData = this.buildPostsGeoJson(stories);
+		if (this.state.firstLoad) {
+			this.fetchStories().then((stories) => {
+				const sourceData = this.buildPostsGeoJson(stories);
 
-			map.on('load', function () {
-				map.addSource('storiesSource', {
-					type: 'geojson',
-					data: sourceData,
-					cluster: true,
-					clusterMaxZoom: 40,
-					clusterRadius: 40,
-				});
-
-				map.loadImage(jeoMapVars.jeoUrl + '/js/src/icons/news-marker.png', function (error, image) {
-					if (error) throw error;
-
-					map.addImage('news-marker', image);
-					map.addLayer({
-						id: 'unclustered-points',
-						type: 'symbol',
-						source: 'storiesSource',
-						filter: ['!', ['has', 'point_count']],
-						layout: {
-							'icon-image': 'news-marker',
-							'icon-size': 0.10,
-							'icon-allow-overlap': true,
-							// 'text-field': 'storie',
-							'text-font': ['Open Sans Bold', 'Arial Unicode MS Bold'],
-							'text-size': 11,
-							'text-transform': 'uppercase',
-							'text-letter-spacing': 0.05,
-							'text-offset': [0, 3],
-						},
-						paint: {
-							'text-color': '#202',
-							'text-halo-color': '#fff',
-							'text-halo-width': 2,
-						},
+				map.on('load', function () {
+					map.addSource('storiesSource', {
+						type: 'geojson',
+						data: sourceData,
+						cluster: true,
+						clusterMaxZoom: 40,
+						clusterRadius: 40,
 					});
-				});
 
-				map.loadImage(jeoMapVars.jeoUrl + '/js/src/icons/news.png', function (error, image) {
-					if (error) throw error;
+					map.loadImage(jeoMapVars.jeoUrl + '/js/src/icons/news-marker.png', function (error, image) {
+						if (error) throw error;
 
-					map.addImage('news', image);
-
-					map.addLayer({
-						id: 'cluster-count',
-						type: 'symbol',
-						source: 'storiesSource',
-
-						layout: {
-							'icon-image': 'news',
-							'icon-size': 0.13,
-							'icon-allow-overlap': true,
-							'icon-offset':  { stops: [
-								[13, [0, -30]],
-								[17, [0, -90]]
-							]},
-							'text-field': '{point_count}',
-							'text-font': ['Open Sans Regular', 'Arial Unicode MS Regular'],
-							'text-size': 12,
-							'text-transform': 'uppercase',
-							'text-letter-spacing': 0.05,
-							'text-offset': [0, 0.8],
-						},
-
-						paint: {
-							'text-color': '#202',
-						},
-
-						filter: ['has', 'point_count'],
+						map.addImage('news-marker', image);
+						map.addLayer({
+							id: 'unclustered-points',
+							type: 'symbol',
+							source: 'storiesSource',
+							filter: ['!', ['has', 'point_count']],
+							layout: {
+								'icon-image': 'news-marker',
+								'icon-size': 0.10,
+								'icon-allow-overlap': true,
+								// 'text-field': 'story',
+								'text-font': ['Open Sans Bold', 'Arial Unicode MS Bold'],
+								'text-size': 11,
+								'text-transform': 'uppercase',
+								'text-letter-spacing': 0.05,
+								'text-offset': [0, 3],
+							},
+							paint: {
+								'text-color': '#202',
+								'text-halo-color': '#fff',
+								'text-halo-width': 2,
+							},
+						});
 					});
-				});
 
-				const layers = [
-					// [6, '#000000'],
-					// [5, '#f28cb1'],
-					// [2, '#f1f075'],
-					[0, '#ffffff'],
-				];
+					map.loadImage(jeoMapVars.jeoUrl + '/js/src/icons/news.png', function (error, image) {
+						if (error) throw error;
 
-				layers.forEach(function (layer, i) {
-					map.addLayer({
-						id: 'cluster-' + i,
-						type: 'circle',
-						source: 'storiesSource',
-						paint: {
-							'circle-color': layer[1],
-							'circle-radius': 20 + layer[0],
-							'circle-stroke-color': '#ffffff',
-							'circle-stroke-opacity': 0.4,
-							'circle-stroke-width': 9,
-						},
-						filter:
-							i === 0
-								? ['>=', 'point_count', layer[0]]
-								: [
-									'all',
-									['>=', 'point_count', layer[0]],
-									['<', 'point_count', layers[i - 1][0]],
-								],
+						map.addImage('news', image);
+
+						map.addLayer({
+							id: 'cluster-count',
+							type: 'symbol',
+							source: 'storiesSource',
+
+							layout: {
+								'icon-image': 'news',
+								'icon-size': 0.13,
+								'icon-allow-overlap': true,
+								'icon-offset':  { stops: [
+									[13, [0, -30]],
+									[17, [0, -90]]
+								]},
+								'text-field': '{point_count}',
+								'text-font': ['Open Sans Regular', 'Arial Unicode MS Regular'],
+								'text-size': 12,
+								'text-transform': 'uppercase',
+								'text-letter-spacing': 0.05,
+								'text-offset': [0, 0.8],
+							},
+
+							paint: {
+								'text-color': '#202',
+							},
+
+							filter: ['has', 'point_count'],
+						});
+					});
+
+					const layers = [
+						// [6, '#000000'],
+						// [5, '#f28cb1'],
+						// [2, '#f1f075'],
+						[0, '#ffffff'],
+					];
+
+					layers.forEach(function (layer, i) {
+						map.addLayer({
+							id: 'cluster-' + i,
+							type: 'circle',
+							source: 'storiesSource',
+							paint: {
+								'circle-color': layer[1],
+								'circle-radius': 20 + layer[0],
+								'circle-stroke-color': '#ffffff',
+								'circle-stroke-opacity': 0.4,
+								'circle-stroke-width': 9,
+							},
+							filter:
+								i === 0
+									? ['>=', 'point_count', layer[0]]
+									: [
+										'all',
+										['>=', 'point_count', layer[0]],
+										['<', 'point_count', layers[i - 1][0]],
+									],
+						});
 					});
 				});
 			});
 
-			// stories.forEach( ( story ) => {
-			// 	const storyPoints = story.meta._related_point;
-
-			// 	if ( storyPoints.length ) {
-			// 		storyPoints.forEach( ( point ) => {
-			// 			this.addMarkerToMap( point );
-			// 		} );
-			// 	}
-			// } );
-		});
+			this.setState({
+				...this.state,
+				firstLoad: false
+			})
+		}
 	}
 
-	fetchStories() {
-		return fetch(jeoMapVars.jsonUrl + 'posts')
-			.then((response) => response.json())
-			.then(
-				(stories) => {
-					this.setState({
-						storiesLoaded: true,
-						stories,
-					});
+	fetchStories(params = {}) {
+		// Use constant POSTS_PER_PAGE if param per_page is not set
+		if(!params.hasOwnProperty('per_page')) params.per_page = POSTS_PER_PAGE;
 
-					return Promise.resolve(stories);
+		// Update using cumulative param for stories infinit scrolling
+		if(params.hasOwnProperty('cumulative') && params.cumulative) {
+			params.page = this.state.currentPage;
+			params.page++;
+
+			this.setState(( state ) => ( {
+				...state,
+				currentPage: params.page,
+			} ))
+		};
+
+		// Cancel request if page exceed the max page;
+		if ( params.page > this.state.totalPages ) {
+			return Promise.reject();
+		}
+
+
+		const url = new URL(jeoMapVars.jsonUrl + 'posts/');
+		Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+
+		return fetch(url)
+			.then((response) => {
+				this.setState(( state ) => ({
+					...state,
+					totalPages: response.headers.get('X-WP-TotalPages'),
+					totalPosts: response.headers.get('X-WP-Total'),
+				} ) );
+
+				return response;
+			})
+			.then((response) => response.json())
+			.then((stories) => {
+					const geolocatedStories = stories.filter( ( story ) => story.meta._related_point.length > 0);
+					const storiesCumulative = params.cumulative? [ ...this.state.stories, ...geolocatedStories ] : geolocatedStories;
+
+					this.setState( ( state ) => ( {
+						...state,
+						storiesLoaded: true,
+						stories: storiesCumulative,
+					} ) );
+
+					return Promise.resolve(storiesCumulative);
 				},
 				(error) => {
 					console.log(error);
 
-					this.setState({
+					this.setState( ( state ) => ({
+						...state,
 						storiesLoaded: false,
-					});
+					} ) );
 				}
 			);
 	}
@@ -201,17 +240,17 @@ class Discorevy extends Component {
 			features: [],
 		};
 
-		stories.map((storie) => {
-			const storieRelatedPoints = storie.meta._related_point ?? [];
-			const storiePoints = storieRelatedPoints.map((point) => {
+		stories.map((story) => {
+			const storyRelatedPoints = story.meta._related_point ?? [];
+			const storyPoints = storyRelatedPoints.map((point) => {
 				return [point._geocode_lon, point._geocode_lat];
 			});
 
 			finalFeatures.features.push(
-				...storiePoints.map((point) => {
+				...storyPoints.map((point) => {
 					return {
 						type: 'Feature',
-						properties: storie,
+						properties: story,
 						geometry: {
 							type: 'Point',
 							coordinates: point,
@@ -224,13 +263,23 @@ class Discorevy extends Component {
 		return finalFeatures;
 	}
 
+	updateStories(params) {
+		// console.log('updateStories');
+
+		this.fetchStories({ ...params }).then((stories) => {
+			const sourceData = this.buildPostsGeoJson(stories);
+			this.map.getSource('storiesSource').setData(sourceData);
+		}).catch( () => "")
+	}
+
 	render() {
 		return (
 			<div className="discovery">
-				<Sidebar />
+				<Sidebar stories={ this.state.stories } updateStories={ this.updateStories } />
+
 				<div
 					ref={ (el) => (this.mapContainer = el) }
-					style={ { height: 'calc(100vh - 200px)' } }
+					style={ { height: 'calc(100vh - 100px)' } }
 					className="discovery-map"
 				/>
 			</div>
@@ -238,4 +287,4 @@ class Discorevy extends Component {
 	}
 }
 
-wp.element.render(<Discorevy />, document.getElementById('post-10'));
+wp.element.render(<Discovery />, document.getElementById('post-10'));
