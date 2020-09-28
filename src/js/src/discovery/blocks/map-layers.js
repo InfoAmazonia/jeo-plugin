ae81359727f0bc118435c883b286de84f450e004
Isaque Melo <isaquegbmelo2@gmail.com>
2020-09-28 14:56:30 -0300

    Add selected layers area

--- src/js/src/discovery/blocks/map-layers.js
+++ src/js/src/discovery/blocks/map-layers.js
@@ -83,6 +83,16 @@ class MapLayers extends Component {
 				<Search searchPlaceholder="Search map" updateStories={ this.props.updateStories } />
 
 				<div className="selected-layers">
+					<div className="status">
+						<div className="status-icon">
+							{/* <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512" class="svg-inline--fa fa-times fa-w-11 fa-3x"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" class=""></path></svg> */}
+							<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-check fa-w-16 fa-3x"><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z" class=""></path></svg>
+						</div>
+
+						<div className="status-message">
+							All changes applied
+						</div>
+					</div>
 					<div className="selected-layers--title"> { __("Selected layers") } </div>
 					<div className="selected-layers--content">
 						{ Object.keys(this.props.selectedLayers).length > 0?
