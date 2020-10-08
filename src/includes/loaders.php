2a1d5caee102719b12233d286eadd39cf700818f
pame <pamela.rosales@pucp.pe>
2020-10-08 13:40:45 -0300

    function exists color_luminance - fix conflict with jeo theme

--- src/includes/loaders.php
+++ src/includes/loaders.php
@@ -255,19 +255,20 @@ function jeo_scripts_typography() {
 }
 add_action( 'wp_enqueue_scripts', 'jeo_scripts_typography' );
 
+if (!function_exists("color_luminance")){
+	function color_luminance($hexcolor, $percent) {
+		if (strlen($hexcolor) < 6) {
+			$hexcolor = $hexcolor[0] . $hexcolor[0] . $hexcolor[1] . $hexcolor[1] . $hexcolor[2] . $hexcolor[2];
+		}
+		$hexcolor = array_map('hexdec', str_split(str_pad(str_replace('#', '', $hexcolor), 6, '0'), 2));
 
-function color_luminance($hexcolor, $percent) {
-	if (strlen($hexcolor) < 6) {
-		$hexcolor = $hexcolor[0] . $hexcolor[0] . $hexcolor[1] . $hexcolor[1] . $hexcolor[2] . $hexcolor[2];
+		foreach ($hexcolor as $i => $color) {
+			$from = $percent < 0 ? 0 : $color;
+			$to = $percent < 0 ? $color : 255;
+			$pvalue = ceil(($to - $from) * $percent);
+			$hexcolor[$i] = str_pad(dechex($color + $pvalue), 2, '0', STR_PAD_LEFT);
+		}
+
+		return '#' . implode($hexcolor);
 	}
-	$hexcolor = array_map('hexdec', str_split(str_pad(str_replace('#', '', $hexcolor), 6, '0'), 2));
-
-	foreach ($hexcolor as $i => $color) {
-		$from = $percent < 0 ? 0 : $color;
-		$to = $percent < 0 ? $color : 255;
-		$pvalue = ceil(($to - $from) * $percent);
-		$hexcolor[$i] = str_pad(dechex($color + $pvalue), 2, '0', STR_PAD_LEFT);
-	}
-
-	return '#' . implode($hexcolor);
 }
