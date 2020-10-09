a1097be914ccd992907277f579bcc99cba328196
Isaque Melo <isaquegbmelo2@gmail.com>
2020-10-09 14:33:44 -0300

    Add mobile message: not supported

--- src/templates/discovery.php
+++ src/templates/discovery.php
@@ -3,6 +3,13 @@ get_header(); ?>
 <main>
 	<div id="discovery">
 	</div>
+
+	<div class="discovery-mobile-warning">
+		<?= __("Sorry! This page can't be viewed on mobiles.", "jeo")?>
+		<a href="/">
+			<?= __("Back to homepage", "jeo") ?>
+		</a>
+	</div>
 </main>
 
 <?php
