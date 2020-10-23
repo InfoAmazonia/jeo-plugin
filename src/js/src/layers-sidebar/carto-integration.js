db61b6d0959b141a5dc60c73ed1e2c2dcb31718a
Isaque Melo <isaquegbmelo2@gmail.com>
2020-10-23 15:28:29 -0300

    Add initial integration

--- src/js/src/layers-sidebar/carto-integration.js
+++ src/js/src/layers-sidebar/carto-integration.js
@@ -1,4 +1,4 @@
-import { Button, SelectControl, CheckboxControl, TextareaControl } from '@wordpress/components';
+import { Button, SelectControl, CheckboxControl, TextareaControl, Spinner, Icon } from '@wordpress/components';
 import { withDispatch, withSelect } from '@wordpress/data';
 import {
 	Fragment,
@@ -14,8 +14,52 @@ import { __ } from '@wordpress/i18n';
 const CartoIntegration = ( { postMeta, setPostMeta }) => {
 	const [ useCartoIntegration, setUseCartoInteration ] = useState(postMeta.use_carto_integration)
 	const [ cartoSQLQuery, setCartoSQLQuery ] = useState(postMeta.carto_integration_sql);
+	const [ processingGeoJsonStatus, setProcessingGeoJsonStatus ] = useState( { doneGeoJsonStage: false, doingGeoJsonStage: false, error: false } );
 
-	// console.log(postMeta);
+	const cartoIntegrationProcess = () => {
+		// console.log("Starting carto integration process");
+		const url = `${window.location.origin}/wp-json/jeowp/carto_integrate`;
+		setProcessingGeoJsonStatus({ doneGeoJsonStage: false, doingGeoJsonStage: true, error: false })
+
+		fetch(url, {
+			method: 'POST',
+			headers: {
+				'X-WP-Nonce': jeo_settings.nonce,
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				sql_query: cartoSQLQuery,
+			})
+		}).then(response => response.json()).then(data => {
+			// console.log("Done, data bellow:");
+			let retrievedUploudObject = false;
+
+			if (data.hasOwnProperty('error')) {
+				setProcessingGeoJsonStatus({ doneGeoJsonStage: false, doingGeoJsonStage: false, error: data.error })
+			} else {
+				setProcessingGeoJsonStatus({ doneGeoJsonStage: true, doingGeoJsonStage: false, error: false })
+				retrievedUploudObject = data.content;
+			}
+
+			if(retrievedUploudObject) {
+				retrievedUploudObject = JSON.parse(retrievedUploudObject);
+
+				const timerId = setInterval(() => {
+					const owner = retrievedUploudObject.owner;
+					const id = retrievedUploudObject.id;
+					const uploudStatusUrl = `https://api.mapbox.com/uploads/v1/${owner}/${id}?access_token=${jeo_settings.mapbox_key}`;
+
+					fetch(uploudStatusUrl).then(uploudResponse => uploudResponse.json()).then( uploudData => {
+						console.log(uploudData);
+						if(uploudData.complete) {
+							clearInterval(timerId);
+						}
+					})
+				}, 5000);
+			}
+		})
+		;
+	}
 
 	return (
 		<>
@@ -46,12 +90,23 @@ const CartoIntegration = ( { postMeta, setPostMeta }) => {
 						type="textarea"
 					/>
 					{ cartoSQLQuery.length > 0 &&
-						<Button isSecondary onClick={ () => {
-
-						} }>
+						<Button isSecondary onClick={ cartoIntegrationProcess } disabled={ processingGeoJsonStatus.doingGeoJsonStage }>
 							{ __("Syncronize") }
 						</Button>
 					}
+					<div className="migration-status">
+						{ !processingGeoJsonStatus.doneGeoJsonStage &&  processingGeoJsonStatus.doingGeoJsonStage && <Spinner /> }
+						{ processingGeoJsonStatus.doneGeoJsonStage && !processingGeoJsonStatus.doingGeoJsonStage &&
+							<>
+								<p> { __("Succesfull tileset migration.") } </p>
+								<p> { __("Uplouding file to Mapbox.") } </p>
+							</>
+						}
+
+						{ (!processingGeoJsonStatus.doneGeoJsonStage && !processingGeoJsonStatus.doingGeoJsonStage && processingGeoJsonStatus.error) &&
+							<span> Error: { processingGeoJsonStatus.error } </span>
+						}
+					</div>
 
 				</>
 			}
