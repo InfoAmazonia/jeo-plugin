6b4e8480380cf125ec12b147f026c15f87dcd42d
Isaque Melo <isaquegbmelo2@gmail.com>
2020-11-23 14:25:42 -0300

    Add toggle status message

--- src/js/src/map-blocks/layer-settings.js
+++ src/js/src/map-blocks/layer-settings.js
@@ -199,60 +199,66 @@ const LayerSettings = (
 					</div>
 
 					{ settings.load_as_style && showStyleLayers && (
-						<div className="layers-selector">
-							{ settings.style_layers &&
-								settings.style_layers.map( ( styleLayer ) => {
-									return (
-										<div key={ styleLayer.id } className="style-layer">
-											<button
-												onClick={ () =>
-													toggleStyleLayerDisplay( styleLayer.id )
-												}
-												className={
-													styleLayer.show
-														? 'toggle-button active'
-														: 'toggle-button'
-												}
-											>
-												{ styleLayer.show ? (
-													<svg
-														aria-hidden="true"
-														focusable="false"
-														data-prefix="fas"
-														data-icon="toggle-on"
-														role="img"
-														xmlns="http://www.w3.org/2000/svg"
-														viewBox="0 0 576 512"
-													>
-														<path
-															fill="currentColor"
-															d="M384 64H192C86 64 0 150 0 256s86 192 192 192h192c106 0 192-86 192-192S490 64 384 64zm0 320c-70.8 0-128-57.3-128-128 0-70.8 57.3-128 128-128 70.8 0 128 57.3 128 128 0 70.8-57.3 128-128 128z"
-														></path>
-													</svg>
-												) : (
-													<svg
-														aria-hidden="true"
-														focusable="false"
-														data-prefix="fas"
-														data-icon="toggle-off"
-														role="img"
-														xmlns="http://www.w3.org/2000/svg"
-														viewBox="0 0 576 512"
-													>
-														<path
-															fill="currentColor"
-															d="M384 64H192C85.961 64 0 149.961 0 256s85.961 192 192 192h192c106.039 0 192-85.961 192-192S490.039 64 384 64zM64 256c0-70.741 57.249-128 128-128 70.741 0 128 57.249 128 128 0 70.741-57.249 128-128 128-70.741 0-128-57.249-128-128zm320 128h-48.905c65.217-72.858 65.236-183.12 0-256H384c70.741 0 128 57.249 128 128 0 70.74-57.249 128-128 128z"
-														></path>
-													</svg>
-												) }
-												<span className="style-layer-title">
-													{ styleLayer.id }
-												</span>
-											</button>
-										</div>
-									);
-								} ) }
-						</div>
+						<>
+							<div className="informative-message">
+								{ __("If the layer is not visible inside Mapbox Studio we will not be able to control the displayment. ", "jeo") }
+							</div>
+
+							<div className="layers-selector">
+								{ settings.style_layers &&
+									settings.style_layers.map( ( styleLayer ) => {
+										return (
+											<div key={ styleLayer.id } className="style-layer">
+												<button
+													onClick={ () =>
+														toggleStyleLayerDisplay( styleLayer.id )
+													}
+													className={
+														styleLayer.show
+															? 'toggle-button active'
+															: 'toggle-button'
+													}
+												>
+													{ styleLayer.show ? (
+														<svg
+															aria-hidden="true"
+															focusable="false"
+															data-prefix="fas"
+															data-icon="toggle-on"
+															role="img"
+															xmlns="http://www.w3.org/2000/svg"
+															viewBox="0 0 576 512"
+														>
+															<path
+																fill="currentColor"
+																d="M384 64H192C86 64 0 150 0 256s86 192 192 192h192c106 0 192-86 192-192S490 64 384 64zm0 320c-70.8 0-128-57.3-128-128 0-70.8 57.3-128 128-128 70.8 0 128 57.3 128 128 0 70.8-57.3 128-128 128z"
+															></path>
+														</svg>
+													) : (
+														<svg
+															aria-hidden="true"
+															focusable="false"
+															data-prefix="fas"
+															data-icon="toggle-off"
+															role="img"
+															xmlns="http://www.w3.org/2000/svg"
+															viewBox="0 0 576 512"
+														>
+															<path
+																fill="currentColor"
+																d="M384 64H192C85.961 64 0 149.961 0 256s85.961 192 192 192h192c106.039 0 192-85.961 192-192S490.039 64 384 64zM64 256c0-70.741 57.249-128 128-128 70.741 0 128 57.249 128 128 0 70.741-57.249 128-128 128-70.741 0-128-57.249-128-128zm320 128h-48.905c65.217-72.858 65.236-183.12 0-256H384c70.741 0 128 57.249 128 128 0 70.74-57.249 128-128 128z"
+															></path>
+														</svg>
+													) }
+													<span className="style-layer-title">
+														{ styleLayer.id }
+													</span>
+												</button>
+											</div>
+										);
+									} ) }
+							</div>
+						</>
 					) }
 				</div>
 			) }
