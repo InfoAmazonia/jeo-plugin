237eb01b859fa2fcd2b07a1e9533ea9833d0c8a3
Isaque Melo <isaquegbmelo2@gmail.com>
2021-01-28 11:44:04 -0300

    Add compact in mobile screens

--- src/js/src/jeo-map/class-jeo-map.js
+++ src/js/src/jeo-map/class-jeo-map.js
@@ -219,15 +219,26 @@ export default class JeoMap {
 								// alert("asdasdas");
 								// console.log(customAttribution);
 
-								map.addControl(
-									new mapboxgl.AttributionControl( {
-										compact: false,
+								let controlPostion = 'bottom-right';
+
+								let attributionControl = new mapboxgl.AttributionControl( {
+									compact: false,
+									customAttribution,
+								} );
+
+								if(window.innerWidth < 600) {
+									attributionControl = new mapboxgl.AttributionControl( {
+										compact: true,
 										customAttribution,
-									} ),
-									'bottom-right'
+									} )
+
+									controlPostion = 'bottom-left';
+								}
+
+								map.addControl(
+									attributionControl,
+									controlPostion
 								);
-
-
 							});
 
 							this.addLayersControl( amountLayers );
