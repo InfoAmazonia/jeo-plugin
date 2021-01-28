dbe212ef497f07e1c339b1234a93efcfe1e5c850
Isaque Melo <isaquegbmelo2@gmail.com>
2021-01-28 13:38:55 -0300

    Add languages / Add wp_set_script_translations

--- src/includes/layer-types/class-layer-types.php
+++ src/includes/layer-types/class-layer-types.php
@@ -124,6 +124,9 @@ class Layer_Types {
 			['mapboxgl-loader'],
 			$asset_file['version']
 		);
+
+		wp_set_script_translations('jeo-layer', 'jeo');
+
 	}
 
 	public function enqueue_scripts() {
@@ -136,6 +139,7 @@ class Layer_Types {
 			$deps = isset( $layer_type['dependecies'] ) ? $layer_type['dependecies'] : [];
 			$deps = array_merge( ['jeo-layer'], $deps );
 			wp_enqueue_script( 'layer-type-' . $slug, $layer_type['script_url'], $deps );
+			wp_set_script_translations('layer-type-' . $slug, 'jeo');
 		}
 
 	}
