997809d875f099cff9fec5d5fb409f2f6b373165
Isaque Melo <isaquegbmelo2@gmail.com>
2021-03-31 15:31:36 -0300

    Add editor settings to limit toolbar itens

--- src/js/src/map-blocks/storymap-editor.js
+++ src/js/src/map-blocks/storymap-editor.js
@@ -25,6 +25,12 @@ import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 const { map_defaults: mapDefaults } = window.jeo_settings;
 
 
+const editorConfig =  {
+	// plugins: [ 'Paragraph', 'Bold', 'Italic', 'Essentials' ],
+	plugins: [ 'Essentials', 'Autoformat', 'Bold', 'Italic', 'BlockQuote', 'Heading', 'Indent', 'Link', 'List', 'Paragraph', 'TextTransformation' ],
+	toolbar: [ 'undo', 'redo','|', 'bold', 'italic', '|', 'heading', 'paragraph', 'link', 'numberedList', '|', 'blockQuote']
+}
+
 const MapEditor = ( {
 	attributes,
 	setAttributes,
@@ -276,6 +282,7 @@ const MapEditor = ( {
 								<CKEditor
 									editor={ ClassicEditor }
 									data={ attributes.description }
+									config={ editorConfig }
 									onChange={ ( event, editor ) =>  {
 										setAttributes( {
 											...attributes,
@@ -379,6 +386,7 @@ const MapEditor = ( {
 															atributo="meuatributo"
 															editor={ ClassicEditor }
 															data={ slide.title }
+															config={ editorConfig }
 															onChange={ ( event, editor ) => {
 																// Set role 'button' to editor element so it isn't affected by drag and drop events
 																editor.ui.getEditableElement().setAttribute('role', 'button')
@@ -400,6 +408,7 @@ const MapEditor = ( {
 														<CKEditor
 															editor={ ClassicEditor }
 															data={ slide.content }
+															config={ editorConfig }
 															onChange={ ( event, editor ) => {
 																// Set role 'button' to editor element so it isn't affected by drag and drop events
 																editor.ui.getEditableElement().setAttribute('role', 'button')
