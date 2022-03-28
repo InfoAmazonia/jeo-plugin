a1df536968e081c311d725191e9627c6d07f8b09
Leonardo <leopiccionia@gmail.com>
2022-03-28 09:58:47 -0300

    Add whitespace

--- src/templates/map-content-layers-list.php
+++ src/templates/map-content-layers-list.php
@@ -3,9 +3,9 @@
 <?php the_content(); ?>
 <?php if(strlen($attribution) > 0): ?>
 	<?php if(strlen($attribution_name) > 0): ?>
-		<?php _e('Attribution: ', 'jeo'); ?><a href="<?php echo $attribution; ?>"><?php echo esc_html( $attribution_name ); ?></a>
+		<?php _e('Attribution: ', 'jeo'); ?> <a href="<?php echo $attribution; ?>"><?php echo esc_html( $attribution_name ); ?></a>
 	<?php else: ?>
-		<?php _e('Attribution: ', 'jeo'); ?><a href="<?php echo $attribution; ?>"><?php echo esc_html( $attribution ); ?></a>
+		<?php _e('Attribution: ', 'jeo'); ?> <a href="<?php echo $attribution; ?>"><?php echo esc_html( $attribution ); ?></a>
 	<?php endif; ?>
 <?php endif; ?>
 <?php if ( $source_url ): ?>
