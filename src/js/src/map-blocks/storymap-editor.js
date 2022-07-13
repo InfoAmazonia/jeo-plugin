76b7492f973c4e58be03b45901804ce6442d67b8
Leonardo <leopiccionia@gmail.com>
2022-07-13 14:10:29 -0300

    Add some default colors

--- src/js/src/map-blocks/storymap-editor.js
+++ src/js/src/map-blocks/storymap-editor.js
@@ -17,6 +17,24 @@ import './storymap-editor.scss';
 
 const { map_defaults: mapDefaults } = window.jeo_settings;
 
+const baseColors = [
+	{ color: 'hsl(0, 0%, 0%)', label: 'Black' },
+	{ color: 'hsl(0, 0%, 30%)', label: 'Dim grey' },
+	{ color: 'hsl(0, 0%, 60%)', label: 'Grey' },
+	{ color: 'hsl(0, 0%, 90%)', label: 'Light grey' },
+	{ color: 'hsl(0, 0%, 100%)', label: 'White', hasBorder: true },
+	{ color: 'hsl(0, 75%, 60%)', label: 'Red' },
+	{ color: 'hsl(30, 75%, 60%)', label: 'Orange' },
+	{ color: 'hsl(60, 75%, 60%)', label: 'Yellow' },
+	{ color: 'hsl(90, 75%, 60%)', label: 'Light green' },
+	{ color: 'hsl(120, 75%, 60%)', label: 'Green' },
+	{ color: 'hsl(150, 75%, 60%)', label: 'Aquamarine' },
+	{ color: 'hsl(180, 75%, 60%)', label: 'Turquoise' },
+	{ color: 'hsl(210, 75%, 60%)', label: 'Light blue' },
+	{ color: 'hsl(240, 75%, 60%)', label: 'Blue' },
+	{ color: 'hsl(270, 75%, 60%)', label: 'Purple' },
+]
+
 const StoryMapEditor = ( {
 	attributes,
 	setAttributes,
@@ -26,17 +44,16 @@ const StoryMapEditor = ( {
 	loadingMap,
 	themeColors,
 } ) => {
-	const colors = ( themeColors || [] ).map( ( color ) => ( {
-		label: color.name,
-		color: color.color,
-	} ) );
+	const colors = [
+		...baseColors,
+		...( themeColors || [] ).map( ( color ) => ( {
+			label: color.name,
+			color: color.color,
+			hasBorder: true,
+		} ) )
+	];
 	const editorConfig = {
-		toolbar: [
-			'undo', 'redo', '|',
-			'bold', 'italic', '|',
-			'heading', 'paragraph', 'link', 'bulletedList', 'numberedList', '|',
-			'fontColor', 'fontBackgroundColor',
-		],
+		toolbar: [ 'undo', 'redo', '|', 'bold', 'italic', '|', 'heading', 'paragraph', 'link', 'bulletedList', 'numberedList', '|', 'fontColor', 'fontBackgroundColor' ],
 		fontBackgroundColor: { colors },
 		fontColor: { colors },
 	};
