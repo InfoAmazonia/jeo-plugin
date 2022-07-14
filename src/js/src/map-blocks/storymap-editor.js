b75e001748b5c7dc88c055ba8469f6a96254742b
Leonardo <leopiccionia@gmail.com>
2022-07-14 10:52:02 -0300

    Add color edition to CKEditor

--- src/js/src/map-blocks/storymap-editor.js
+++ src/js/src/map-blocks/storymap-editor.js
@@ -1,34 +1,39 @@
-import {
-	Button,
-	Spinner,
-	CheckboxControl,
-	Dashicon,
-	Panel,
-	PanelBody,
-} from '@wordpress/components';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import { Button, CheckboxControl, Dashicon, Panel, PanelBody, Spinner } from '@wordpress/components';
 import { compose, withInstanceId } from '@wordpress/compose';
 import { withSelect, select } from '@wordpress/data';
 import { Fragment, useState, useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import ClassicEditor from 'ckeditor5-build-full';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import { List, arrayMove } from 'react-movable';
 
 import Map from './map';
 import { renderLayer } from './map-preview-layer';
 import JeoAutosuggest from './jeo-autosuggest';
+import JeoGeoAutoComplete from '../posts-sidebar/geo-auto-complete';
 import './map-editor.css';
 import './storymap-editor.scss';
-import { List, arrayMove } from 'react-movable';
-import JeoGeoAutoComplete from '../posts-sidebar/geo-auto-complete';
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 const { map_defaults: mapDefaults } = window.jeo_settings;
 
-const editorConfig =  {
-	// plugins: [ 'Paragraph', 'Bold', 'Italic', 'Essentials' ],
-	plugins: [ 'Essentials', 'Autoformat', 'Bold', 'Italic', 'BlockQuote', 'Heading', 'Indent', 'Link', 'List', 'Paragraph', 'TextTransformation' ],
-	toolbar: [ 'undo', 'redo','|', 'bold', 'italic', '|', 'heading', 'paragraph', 'link', 'bulletedList', 'numberedList']
-}
+const baseColors = [
+	{ color: 'hsl(0, 0%, 0%)', label: 'Black' },
+	{ color: 'hsl(0, 0%, 30%)', label: 'Dim grey' },
+	{ color: 'hsl(0, 0%, 60%)', label: 'Grey' },
+	{ color: 'hsl(0, 0%, 90%)', label: 'Light grey' },
+	{ color: 'hsl(0, 0%, 100%)', label: 'White', hasBorder: true },
+	{ color: 'hsl(0, 75%, 60%)', label: 'Red' },
+	{ color: 'hsl(30, 75%, 60%)', label: 'Orange' },
+	{ color: 'hsl(60, 75%, 60%)', label: 'Yellow' },
+	{ color: 'hsl(90, 75%, 60%)', label: 'Light green' },
+	{ color: 'hsl(120, 75%, 60%)', label: 'Green' },
+	{ color: 'hsl(150, 75%, 60%)', label: 'Aquamarine' },
+	{ color: 'hsl(180, 75%, 60%)', label: 'Turquoise' },
+	{ color: 'hsl(210, 75%, 60%)', label: 'Light blue' },
+	{ color: 'hsl(240, 75%, 60%)', label: 'Blue' },
+	{ color: 'hsl(270, 75%, 60%)', label: 'Purple' },
+]
 
 const StoryMapEditor = ( {
 	attributes,
@@ -37,7 +42,22 @@ const StoryMapEditor = ( {
 	loadedLayers,
 	loadedMap,
 	loadingMap,
+	themeColors,
 } ) => {
+	const colors = [
+		...baseColors,
+		...( themeColors || [] ).map( ( color ) => ( {
+			label: color.name,
+			color: color.color,
+			hasBorder: true,
+		} ) )
+	];
+	const editorConfig = {
+		toolbar: [ 'undo', 'redo', '|', 'bold', 'italic', '|', 'heading', 'paragraph', 'link', 'bulletedList', 'numberedList', '|', 'fontColor', 'fontBackgroundColor' ],
+		fontBackgroundColor: { colors },
+		fontColor: { colors },
+	};
+
 	const decodeHtmlEntity = function ( str ) {
 		return str.replace( /&#(\d+);/g, function ( match, dec ) {
 			return String.fromCharCode( dec );
@@ -834,6 +854,7 @@ const applyWithSelect = withSelect( ( select, { attributes } ) => ( {
 		'getEntityRecords',
 		[ 'postType', 'map-layer' ]
 	),
+	themeColors: select( 'core/editor' ).getEditorSettings().colors,
 } ) );
 
 export default compose( withInstanceId, applyWithSelect )( StoryMapEditor );
