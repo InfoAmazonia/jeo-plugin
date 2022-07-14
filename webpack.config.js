30af26c261811f1f15b18a522968ff2cdaa54c2b
Leonardo <leopiccionia@gmail.com>
2022-07-14 10:52:02 -0300

    Add color edition to CKEditor

--- webpack.config.js
+++ webpack.config.js
@@ -28,18 +28,10 @@ module.exports = {
 				test: /\.css$/,
 				use: [ 'style-loader', 'css-loader' ],
 			},
-
 			{
 				test: /\.s[ac]ss$/i,
-				use: [
-				  // Creates `style` nodes from JS strings
-				  'style-loader',
-				  // Translates CSS into CommonJS
-				  'css-loader',
-				  // Compiles Sass to CSS
-				  'sass-loader',
-				],
-			  },
+				use: [ 'style-loader', 'css-loader', 'sass-loader' ],
+			},
 		],
 	},
 };
