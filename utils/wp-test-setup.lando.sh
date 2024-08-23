584bcffd182803cee091c3b9ecaa131a0e5bec91
leopiccionia <leopiccionia@gmail.com>
2024-08-23 12:22:56 -0300

    Improve README and remove unused files

--- utils/wp-test-setup.lando.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Sets up the test environment
-set -o errexit
-set -o nounset
-set -o pipefail
-#set -o xtrace
-
-sed "s_/tmp/wordpress/_$(pwd)/test-environment/_" tests/bootstrap-config-sample.php > tests/bootstrap-config.php
