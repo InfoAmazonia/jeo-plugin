2ab0f0f66728de1d64a6f1cc99b260488318ba90
leopiccionia <leopiccionia@gmail.com>
2024-09-19 18:40:53 -0300

    Remove some bundle duplication
    Dependency 'mapbox-gl' was bundled many times in same page.
    Now it'll bundled less often

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,26 @@ module.exports = {
 	...defaultConfig,
 	entry: {
 		mapboxglLoader: './src/js/src/mapboxgl-loader.js',
-		jeoMap: './src/js/src/jeo-map/index.js',
+		jeoMap: {
+			import: './src/js/src/jeo-map/index.js',
+			dependOn: ['mapboxglLoader'],
+		},
 		JeoLayer: './src/includes/layer-types/JeoLayer.js',
 		JeoLegend: './src/includes/legend-types/JeoLegend.js',
-		mapBlocks: './src/js/src/map-blocks/index.js',
+		mapBlocks: {
+			import: './src/js/src/map-blocks/index.js',
+			dependOn: ['mapboxglLoader'],
+		},
 		discovery: './src/js/src/discovery/index.js',
 		// storymap: './src/js/src/map-blocks/storymap.js',
-		layersSidebar: './src/js/src/layers-sidebar/index.js',
-		mapsSidebar: './src/js/src/maps-sidebar/index.js',
+		layersSidebar: {
+			import: './src/js/src/layers-sidebar/index.js',
+			dependOn: ['mapboxglLoader'],
+		},
+		mapsSidebar: {
+			import: './src/js/src/maps-sidebar/index.js',
+			dependOn: ['mapboxglLoader'],
+		},
 		postsSidebar: './src/js/src/posts-sidebar/index.js',
 	},
 	output: {
