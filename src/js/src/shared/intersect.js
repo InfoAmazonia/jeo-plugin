b310bac300a3215ee85d99f477a4534ea0a8d7bf
leopiccionia <leopiccionia@gmail.com>
2024-09-26 16:26:17 -0300

    Remove console logs

--- src/js/src/shared/intersect.js
+++ src/js/src/shared/intersect.js
@@ -4,13 +4,11 @@ export function onFirstIntersection ( el, callback ) {
 	const { innerHeight: windowHeight, innerWidth: windowWidth } = window;
 
 	if ( top >= 0 && left >= 0 && bottom <= ( windowHeight || clientHeight ) && right <= ( windowWidth || clientWidth ) ) {
-		console.debug( 'eager init', el );
 		callback();
 	} else {
 		let observer;
 		const innerCallback = ([ intersectionEntry ]) => {
 			if ( intersectionEntry?.isIntersecting ) {
-				console.debug( 'lazy init', el );
 				callback();
 				observer?.unobserve( el );
 			}
