325729cd7352a185bb71fd706ad1d88fed338aa5
leopiccionia <leopiccionia@gmail.com>
2025-09-05 12:42:37 -0300

    Fix marker loading

--- src/js/src/lib/mapgl-loader.js
+++ src/js/src/lib/mapgl-loader.js
@@ -3,10 +3,10 @@ __webpack_public_path__ = jeo_settings.public_path
 /** @type string */
 export const MAP_RUNTIME = jeo_settings.map_runtime
 
-const { createMap, defaultStyle, mapboxToken, mapgl } = await (
+const { createMap, defaultStyle, loadImage, mapboxToken, mapgl } = await (
 	(MAP_RUNTIME === 'maplibregl')
 		? import(/* webpackChunkName: "maplibreglLoader" */ './maplibregl/maplibregl-loader.js')
 		: import(/* webpackChunkName: "mapboxglLoader" */ './mapboxgl/mapboxgl-loader.js')
 )
 
-export { createMap, defaultStyle, mapboxToken, mapgl }
+export { createMap, defaultStyle, loadImage, mapboxToken, mapgl }
