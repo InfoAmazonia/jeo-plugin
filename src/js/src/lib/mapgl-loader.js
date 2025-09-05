4019ad886457a11503917456be89f650350d5258
leopiccionia <leopiccionia@gmail.com>
2025-09-05 18:09:01 -0300

    Add compat layer for some cluster functions

--- src/js/src/lib/mapgl-loader.js
+++ src/js/src/lib/mapgl-loader.js
@@ -3,10 +3,26 @@ __webpack_public_path__ = jeo_settings.public_path
 /** @type string */
 export const MAP_RUNTIME = jeo_settings.map_runtime
 
-const { createMap, defaultStyle, loadImage, mapboxToken, mapgl } = await (
+const {
+	createMap,
+	defaultStyle,
+	getClusterExpansionZoom,
+	getClusterLeaves,
+	loadImage,
+	mapboxToken,
+	mapgl,
+} = await (
 	(MAP_RUNTIME === 'maplibregl')
 		? import(/* webpackChunkName: "maplibreglLoader" */ './maplibregl/maplibregl-loader.js')
 		: import(/* webpackChunkName: "mapboxglLoader" */ './mapboxgl/mapboxgl-loader.js')
 )
 
-export { createMap, defaultStyle, loadImage, mapboxToken, mapgl }
+export {
+	createMap,
+	defaultStyle,
+	getClusterExpansionZoom,
+	getClusterLeaves,
+	loadImage,
+	mapboxToken,
+	mapgl,
+}
