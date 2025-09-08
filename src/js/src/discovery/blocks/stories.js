c173aea122fd790382880604a6a3e40b60a805fe
leopiccionia <leopiccionia@gmail.com>
2025-09-08 18:27:00 -0300

    Use non-deprecated expressions

--- src/js/src/discovery/blocks/stories.js
+++ src/js/src/discovery/blocks/stories.js
@@ -109,17 +109,7 @@ class Stories extends Component {
 								'icon-image': 'news-marker',
 								'icon-size': 0.1,
 								'icon-allow-overlap': true,
-								// 'text-field': 'story',
-								// 'text-font': ['Open Sans Bold'],
-								// 'text-size': 11,
-								// 'text-transform': 'uppercase',
-								// 'text-letter-spacing': 0.05,
-								// 'text-offset': [0, 3],
 							},
-
-							// paint : {
-							// 	'icon-opacity': 1,
-							// }
 						} );
 
 						loadImage( map, 'news-marker-hover', jeoMapVars.jeoUrl + '/js/src/icons/news-marker-hover.png' )
@@ -149,35 +139,19 @@ class Stories extends Component {
 
 
 						loadImage( map, 'news-no-marker', jeoMapVars.jeoUrl + '/js/src/icons/news.png' ).then( () => {
-							const layers = [
-								// [6, '#000000'],
-								// [5, '#f28cb1'],
-								// [2, '#f1f075'],
-								[ 0, '#ffffff' ],
-							];
-
 							// cluster circle layer
-							layers.forEach( ( layer, i ) => {
-								map.addLayer( {
-									id: 'cluster-' + i,
-									type: 'circle',
-									source: 'storiesSource',
-									paint: {
-										'circle-color': layer[ 1 ],
-										'circle-radius': 20 + layer[ 0 ],
-										'circle-stroke-color': '#ffffff',
-										'circle-stroke-opacity': 0.4,
-										'circle-stroke-width': 9,
-									},
-									filter:
-										i === 0
-											? [ '>=', 'point_count', layer[ 0 ] ]
-											: [
-													'all',
-													[ '>=', 'point_count', layer[ 0 ] ],
-													[ '<', 'point_count', layers[ i - 1 ][ 0 ] ],
-											  ],
-								} );
+							map.addLayer( {
+								id: 'cluster-layer',
+								type: 'circle',
+								source: 'storiesSource',
+								filter: [ 'has', 'point_count' ],
+								paint: {
+									'circle-color': '#ffffff',
+									'circle-radius': 20,
+									'circle-stroke-color': '#ffffff',
+									'circle-stroke-opacity': 0.4,
+									'circle-stroke-width': 9,
+								},
 							} );
 
 							// cluster number layer
@@ -185,7 +159,7 @@ class Stories extends Component {
 								id: 'cluster-count',
 								type: 'symbol',
 								source: 'storiesSource',
-
+								filter: [ 'has', 'point_count' ],
 								layout: {
 									'icon-image': 'news-no-marker',
 									'icon-size': 0.13,
@@ -203,14 +177,10 @@ class Stories extends Component {
 									'text-letter-spacing': 0.05,
 									'text-offset': [ 0, 0.8 ],
 								},
-
 								paint: {
 									'text-color': '#202202',
 								},
-
-								filter: [ 'has', 'point_count' ],
 							} );
-
 						} );
 					}
 				);
