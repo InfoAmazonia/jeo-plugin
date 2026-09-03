997261156a8e82ff34469ff349011abab806fa6d
leopiccionia <leopiccionia@gmail.com>
2026-09-03 15:59:33 -0300

    Chore: Fix Jest

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,12 @@ const baseConfig = require( '@wordpress/scripts/config/jest-unit.config' );
 
 module.exports = {
 	...baseConfig,
+	transform: {
+		...baseConfig.transform,
+		// @wordpress/theme ships ESM-only (.mjs); transform it so Jest can require it.
+		'\\.mjs$': require.resolve( '@wordpress/scripts/config/babel-transform' ),
+	},
 	transformIgnorePatterns: [
-		'/node_modules/(?!.*(uuid|eta/dist/core\\.js|@rjsf/.+|@x0k/json-schema-merge/.+|marked|lodash-es))',
+		'/node_modules/(?!.*(uuid|eta/dist/core\\.js|@rjsf/.+|@x0k/json-schema-merge/.+|marked|lodash-es|@wordpress/theme/))',
 	],
 };
